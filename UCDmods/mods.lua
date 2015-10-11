cde8cc918fa3d877358c454440f2d12eee789331
nokizorque <lewis.watson@outlook.com>
2015-10-11 19:35:12 +1000

    UCDmods
    - Added sick Infernus

--- UCDmods/mods.lua
+++ UCDmods/mods.lua
@@ -6,5 +6,8 @@ function applyMods()
 	engineImportTXD(staff, 217)
 	local staff = engineLoadDFF( "staff.dff", 217 )
 	engineReplaceModel( staff, 217 )
+	
+	engineImportTXD(engineLoadTXD("infernus.txd", true), 411)
+	engineReplaceModel(engineLoadDFF("infernus.dff", 411), 411)
 end
 addEventHandler( "onClientResourceStart", resourceRoot, applyMods )
