c979173850f586263f3ab35d8e624186319dcdd7
nokizorque <lewis.watson@outlook.com>
2015-10-11 21:37:20 +1000

    UCDutil
    - Changed GenerateString to randomstring and made it look cleaner.

--- UCDutil/clientExports.lua
+++ UCDutil/clientExports.lua
@@ -129,14 +129,14 @@ function getElementSpeed(element, unit)
 	end
 end
 
-function GenerateString ( len )
-	local allowed = { { 48, 57 }, { 65, 90 }, { 97, 122 } } -- numbers/lowercase chars/uppercase chars
-    if tonumber ( len ) then
-        math.randomseed ( getTickCount () )
+function randomstring(len)
+	local allowed = {{48, 57}, {65, 90}, {97, 122}} -- numbers/lowercase chars/uppercase chars
+    if tonumber(len) then
+        math.randomseed(getTickCount())
         local str = ""
         for i = 1, len do
-            local charlist = allowed[math.random ( 1, 3 )]
-            str = str .. string.char ( math.random ( charlist[1], charlist[2] ) )
+            local charlist = allowed[math.random(1, 3)]
+            str = str..string.char(math.random(charlist[1], charlist[2]))
         end
         return str
     end
