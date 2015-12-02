29243385f2c1fd011c52f0cffe67cc847b94dbfd
nokizorque <lewis.watson@outlook.com>
2015-12-02 13:00:39 +1000

    UCDutil
    - Fixed confirmation window.
    - Now uses a callback event as opposed to a callback loadstring.

--- UCDutil/confirmation_c.lua
+++ UCDutil/confirmation_c.lua
@@ -1,13 +1,15 @@
-function createConfirmationWindow(text, callbackString)
+function createConfirmationWindow(event, arg, isServerEvent, title, text)
 	local confirmation = {window={}, label={}, button={}}
 	
 	local calledResource = tostring(Resource.getName(sourceResource))
 	-- Replace 'UCD' with blank and capitalise the first letter
-	calledResource = calledResource:gsub("UCD", "")
-	calledResource = calledResource:gsub("^%l", string.upper)
-	outputDebugString(callbackString)
+	if (not title) then
+		calledResource = calledResource:gsub("UCD", "")
+		calledResource = calledResource:gsub("^%l", string.upper)
+		title = "UCD | "..calledResource.." - Confirmation"
+	end
 	
-	confirmation.window[1] = guiCreateWindow(819, 425, 282, 132, "UCD | "..calledResource.." - Confirmation", false)
+	confirmation.window[1] = guiCreateWindow(819, 425, 282, 132, title, false)
 	guiWindowSetSizable(confirmation.window[1], false)
 
 	confirmation.label[1] = guiCreateLabel(10, 26, 262, 44, tostring(text), false, confirmation.window[1])
@@ -25,22 +27,34 @@ function createConfirmationWindow(text, callbackString)
 				confirmation.window[1]:destroy()
 			end
 			
-			if (callbackString ~= "") then
-				loadstring(callbackString)()
+			if (event) then
+				if (isServerEvent and isServerEvent ~= nil) then
+					triggerServerEvent(event, localPlayer, arg)
+				else
+					triggerEvent(event, localPlayer, arg)
+				end
 			end
 			
 			showCursor(false)
 			return true
 		elseif (source == confirmation.button[2]) then
+		
 			if (isElement(confirmation.window[1])) then
 				removeEventHandler("onClientGUIClick", confirmation.window[1], confirmationWindowClick)
 				confirmation.window[1]:destroy()
 			end
+			
 			showCursor(false)
 			return false
 		end
 	end
 	addEventHandler("onClientGUIClick", confirmation.window[1], confirmationWindowClick)
+	
+	if (confirmation.window[1] and isElement(confirmation.window[1])) then
+		return true
+	else
+		return false
+	end
 end
 
 --[[
