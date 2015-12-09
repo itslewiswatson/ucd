db0b859da7278f5488da192b737f7076c74db3bc
nokizorque <lewis.watson@outlook.com>
2015-12-09 16:48:58 +1000

    UCDhud
    - Improved relativity across resolutions.
    - Fixed clock scaling.

--- UCDhud/c_hud.lua
+++ UCDhud/c_hud.lua
@@ -1,4 +1,4 @@
---local sX, sY = 1366, 768
+local sX, sY = 1366, 768
 local sX, sY = guiGetScreenSize()
 local nX, nY = 1366, 768
 local iX, iY = 1920, 1080
@@ -31,6 +31,30 @@ addEventHandler("onClientResourceStop", resourceRoot,
 	end
 )
 
+function renderClock()
+	if (not isPlayerHudComponentVisible("radar") or isPlayerMapVisible()) then return end
+	
+	local h, m = getTime()
+	if m < 10 then
+		m = "0"..m
+	end
+	if h < 10 then
+		h = "0"..h
+	end
+	
+	local scaleX, scaleY
+	scaleX = sX / iX
+	scaleY = sY / iY
+	
+	dxDrawText(h..":"..m, ((1637 / iX) * sX) - 1, ((67 / iY) * sY) - 1, ((1822 / iX) * sX) - 1, ((109 / iY) * sY)- 1, tocolor(0, 0, 0, 255), (scaleX + scaleY) * 2, "default", "right", "bottom", false, false, false, false, false)
+	dxDrawText(h..":"..m, ((1637 / iX) * sX) + 1, ((67 / iY) * sY) - 1, ((1822 / iX) * sX) + 1, ((109 / iY) * sY) - 1, tocolor(0, 0, 0, 255), (scaleX + scaleY) * 2, "default", "right", "bottom", false, false, false, false, false)
+	dxDrawText(h..":"..m, ((1637 / iX) * sX) - 1, ((67 / iY) * sY) + 1, ((1822 / iX) * sX) - 1, ((109 / iY) * sY) + 1, tocolor(0, 0, 0, 255), (scaleX + scaleY) * 2, "default", "right", "bottom", false, false, false, false, false)
+	dxDrawText(h..":"..m, ((1637 / iX) * sX) + 1, ((67 / iY) * sY) + 1, ((1822 / iX) * sX) + 1, ((109 / iY) * sY) + 1, tocolor(0, 0, 0, 255), (scaleX + scaleY) * 2, "default", "right", "bottom", false, false, false, false, false)
+	dxDrawText(h..":"..m, ((1637 / iX) * sX), ((67 / iY) * sY), ((1822 / iX) * sX), ((109 / iY) * sY), tocolor(255, 255, 255, 255), (scaleX + scaleY) * 2, "default", "right", "bottom", false, false, false, false, false)
+	
+end
+addEventHandler("onClientRender", root, renderClock)
+
 -- This is still optimized for 1366x768
 function renderWeapons2()
 	if (not isPlayerHudComponentVisible("radar") or isPlayerMapVisible()) then return end
@@ -51,24 +75,23 @@ function renderWeapons()
 	local wepID = getPedWeapon(localPlayer)
 	local ammoIndicatorText = clipAmmo.."/"..totalAmmo - clipAmmo
 	
-	if (wepSlot == 6) or (wepSlot == 8) or (wepID == 25) or (wepID == 35) or (wepID == 36) then
+	if (wepSlot == 6 or wepSlot == 8 or wepID == 25 or wepID == 35 or wepID == 36) then
 		ammoIndicatorText = tostring(totalAmmo)
 	end
 
-	if (wepSlot == 0) or (wepSlot == 1) or (wepSlot == 10) or (wepSlot == 12) or (wepID == 44) or (wepID == 45) or (wepID == 46) then
+	if (wepSlot == 0 or wepSlot == 1 or wepSlot == 10 or wepSlot == 12 or wepID == 44 or wepID == 45 or wepID == 46) then
 		return
 	end
 	
-	dxDrawText(ammoIndicatorText, ((1078 - 1) / nX) * sX, ((110 - 1) / nY) * sY, ((1140 - 1) / nX) * sX, ((134 - 1) / nY) * sY, tocolor(0, 0, 0, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)
-	dxDrawText(ammoIndicatorText, ((1078 + 1) / nX) * sX, ((110 - 1) / nY) * sY, ((1140 + 1) / nX) * sX, ((134 - 1) / nY) * sY, tocolor(0, 0, 0, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)
-	dxDrawText(ammoIndicatorText, ((1078 - 1) / nX) * sX, ((110 + 1) / nY) * sY, ((1140 - 1) / nX) * sX, ((134 + 1) / nY) * sY, tocolor(0, 0, 0, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)
-	dxDrawText(ammoIndicatorText, ((1078 + 1) / nX) * sX, ((110 + 1) / nY) * sY, ((1140 + 1) / nX) * sX, ((134 + 1) / nY) * sY, tocolor(0, 0, 0, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)
-	dxDrawText(ammoIndicatorText, (1078 / nX) * sX, (110 / nY) * sY, (1140 / nX) * sX, (134 / nY) * sY, tocolor(255, 255, 255, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)	
+	dxDrawText(ammoIndicatorText, ((1078 / nX) * sX) - 1, ((110 / nY) * sY) - 1, ((1140 / nX) * sX) - 1, ((134 / nY) * sY) - 1, tocolor(0, 0, 0, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)
+	dxDrawText(ammoIndicatorText, ((1078 / nX) * sX) + 1, ((110 / nY) * sY) - 1, ((1140 / nX) * sX) + 1, ((134 / nY) * sY) - 1, tocolor(0, 0, 0, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)
+	dxDrawText(ammoIndicatorText, ((1078 / nX) * sX) - 1, ((110 / nY) * sY) + 1, ((1140 / nX) * sX) - 1, ((134 / nY) * sY) + 1, tocolor(0, 0, 0, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)
+	dxDrawText(ammoIndicatorText, ((1078 / nX) * sX) + 1, ((110 / nY) * sY) + 1, ((1140 / nX) * sX) + 1, ((134 / nY) * sY) + 1, tocolor(0, 0, 0, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)
+	dxDrawText(ammoIndicatorText, ((1078 / nX) * sX)	, ((110 / nY) * sY), 	 ((1140 / nX) * sX), 	 ((134 / nY) * sY), 	tocolor(255, 255, 255, 255), 1.50, "default-bold", "center", "top", false, false, false, false, false)	
 end
 addEventHandler("onClientRender", root, renderWeapons)
 
 -- All functions from here on in are optimized for 1920x1080 (the res they were made in)
-
 function renderArmour()
 	if (not isPlayerHudComponentVisible("radar") or isPlayerMapVisible()) then return end	
 	local armourLevel = getPedArmor(localPlayer)
@@ -95,69 +118,55 @@ function renderOxygen()
 end
 addEventHandler("onClientRender", root, renderOxygen)
 
-function renderClock()
-	if (not isPlayerHudComponentVisible("radar") or isPlayerMapVisible()) then return end	
-	local h, m = getTime()
-	if m < 10 then
-		m = "0"..m
-	end
-	if h < 10 then
-		h = "0"..h
-	end
-	
-	dxDrawText(h..":"..m, (1636 / iX) * sX, (66 / iY) * sY, (1821 / iX) * sX, (108 / iY) * sY, tocolor(0, 0, 0, 255), 3.5, "default", "right", "bottom", false, false, false, false, false)
-	dxDrawText(h..":"..m, (1638 / iX) * sX, (66 / iY) * sY, (1823 / iX) * sX, (108 / iY) * sY, tocolor(0, 0, 0, 255), 3.5, "default", "right", "bottom", false, false, false, false, false)
-	dxDrawText(h..":"..m, (1636 / iX) * sX, (68 / iY) * sY, (1821 / iX) * sX, (110 / iY) * sY, tocolor(0, 0, 0, 255), 3.5, "default", "right", "bottom", false, false, false, false, false)
-	dxDrawText(h..":"..m, (1638 / iX) * sX, (68 / iY) * sY, (1823 / iX) * sX, (110 / iY) * sY, tocolor(0, 0, 0, 255), 3.5, "default", "right", "bottom", false, false, false, false, false)
-	dxDrawText(h..":"..m, (1637 / iX) * sX, (67 / iY) * sY, (1822 / iX) * sX, (109 / iY) * sY, tocolor(255, 255, 255, 255), 3.5, "default", "right", "bottom", false, false, false, false, false)
-	
-end
-addEventHandler("onClientRender", root, renderClock)
-
 function renderMoney()
 	if (not isPlayerHudComponentVisible("radar") or isPlayerMapVisible()) then return end
+	local r, g, b
+	local cY, fY
 	local finalOutputM = exports.UCDutil:tocomma(localPlayer:getMoney())
 	
 	if (getPedStat(localPlayer, 24) == 1000) then
-		aY, bY, cY = 208, 210, 209
-		dY, eY, fY = 243, 245, 244
+		cY = 209
+		fY = 244
 	else
-		aY, bY, cY = 182, 184, 183	
-		dY, eY, fY = 217, 219, 218
+		cY = 183	
+		fY = 218
+	end
+	if (localPlayer:getMoney() < 0) then
+		r, g, b = 178, 34, 34
+	else
+		r, g, b = 45, 180, 80
 	end
 	
-	dxDrawText("$"..finalOutputM, (1505 / iX) * sX, (aY / iY) * sY, (1821 / iX) * sX, (dY / iY) * sY, tocolor(0, 0, 0, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
-	dxDrawText("$"..finalOutputM, (1507 / iX) * sX, (aY / iY) * sY, (1823 / iX) * sX, (dY / iY) * sY, tocolor(0, 0, 0, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
-	dxDrawText("$"..finalOutputM, (1506 / iX) * sX, (bY / iY) * sY, (1821 / iX) * sX, (eY / iY) * sY, tocolor(0, 0, 0, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
-	dxDrawText("$"..finalOutputM, (1507 / iX) * sX, (bY / iY) * sY, (1823 / iX) * sX, (dY / iY) * sY, tocolor(0, 0, 0, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
-	dxDrawText("$"..finalOutputM, (1506 / iX) * sX, (cY / iY) * sY, (1822 / iX) * sX, (fY / iY) * sY, tocolor(45, 180, 80, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
+	dxDrawText("$"..finalOutputM, ((1506 / iX) * sX) - 1, ((cY / iY) * sY) - 1, ((1822 / iX) * sX) - 1, ((244 / iY) * sY) - 1, tocolor(0, 0, 0, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
+	dxDrawText("$"..finalOutputM, ((1506 / iX) * sX) + 1, ((cY / iY) * sY) - 1, ((1822 / iX) * sX) + 1, ((244 / iY) * sY) - 1, tocolor(0, 0, 0, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
+	dxDrawText("$"..finalOutputM, ((1506 / iX) * sX) - 1, ((cY / iY) * sY) + 1, ((1822 / iX) * sX) - 1, ((244 / iY) * sY) + 1, tocolor(0, 0, 0, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
+	dxDrawText("$"..finalOutputM, ((1506 / iX) * sX) + 1, ((cY / iY) * sY) + 1, ((1822 / iX) * sX) + 1, ((244 / iY) * sY) + 1, tocolor(0, 0, 0, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
+	dxDrawText("$"..finalOutputM, ((1506 / iX) * sX)	, ((cY / iY) * sY)	  , ((1822 / iX) * sX)    , ((244 / iY) * sY), 	   tocolor(r, g, b, 255), 1.80, "pricedown", "right", "top", false, false, false, false, false)
 end
 addEventHandler("onClientRender", root, renderMoney)
 
 function renderHealthBar()
 	if (not isPlayerHudComponentVisible("radar") or isPlayerMapVisible()) then return end
-	health = getElementHealth(localPlayer)
 	
 	if (getPedStat(localPlayer, 24) == 1000) then
 		dxDrawRectangle((1498 / iX) * sX, (185 / iY) * sY, (323 / iX) * sX, (18 / iY) * sY, tocolor(0, 0, 0, 255), false)
-		dxDrawRectangle((1501 / iX) * sX, (188 / iY) * sY, health * ((1.585 / iX) * sX), (12 / iY) * sY, tocolor(255, 0, 0, 255), false)
+		dxDrawRectangle((1501 / iX) * sX, (188 / iY) * sY, localPlayer.health * ((1.585 / iX) * sX), (12 / iY) * sY, tocolor(255, 0, 0, 255), false)
 	
 	else
 		dxDrawRectangle((1639 / iX) * sX, (156 / iY) * sY, (181 / iX) * sX, (18 / iY) * sY, tocolor(0, 0, 0, 255), false)
-		dxDrawRectangle((1642 / iX) * sX, (159 / iY) * sY, health * ((1.75 / iX) * sX), (12 / iY) * sY, tocolor(255, 0, 0, 255), false)	
+		dxDrawRectangle((1642 / iX) * sX, (159 / iY) * sY, localPlayer.health * ((1.75 / iX) * sX), (12 / iY) * sY, tocolor(255, 0, 0, 255), false)	
 	end
 end
 addEventHandler("onClientRender", root, renderHealthBar)
 
 function renderWanted()
 	if (not isPlayerHudComponentVisible("radar") or isPlayerMapVisible() or localPlayer:getWantedLevel() == 0) then return end
-	local wantedLevel = localPlayer:getWantedLevel()
 	
-	for i=1, wantedLevel do
+	for i = 1, localPlayer:getWantedLevel() do
 		local offset = 34 -- 1920 / 55 (55 being the old offset)
 		-- The distance in between needs to be relative to the screen resolution
 		-- This is level with the rest of the HUD at 1920x1080 and 1366x768
-		dxDrawImage(((1780 / iX) * sX) - ((sX / offset) * (i - 1)), (280 / iY) * sY, (48 / iX) * sX, (48 / iY) * sY, "star.png")--tocolor(255, 255, 255, 255), false)
+		dxDrawImage(((1780 / iX) * sX) - ((sX / offset) * (i - 1)), (280 / iY) * sY, (48 / iX) * sX, (48 / iY) * sY, "star.png")
 	end
 end
 addEventHandler("onClientRender", root, renderWanted)
