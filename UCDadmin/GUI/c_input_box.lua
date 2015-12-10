c65f0980be3a78e2891c3cb21eec3a8515a2d4bd
nokizorque <lewis.watson@outlook.com>
2015-12-10 22:24:23 +1000

    UCDadmin
    - Cleaned up the code a little bit.
    - Removed redundancies.
    - Added createInputBox as an export.

--- UCDadmin/GUI/c_input_box.lua
+++ UCDadmin/GUI/c_input_box.lua
@@ -48,62 +48,14 @@ end
 function clickInputBox(button)
 	if (button == "left") then
 		if (source == confirm.button[1]) then
-			if (action_ == "UCDadmin.giveVehicle" and tonumber(confirm.edit[1]:getText()) == nil) then
-				if (not getVehicleModelFromName(confirm.edit[1]:getText())) then
+			local text = confirm.edit[1].text
+			if (action_ == "UCDadmin.giveVehicle" and tonumber(text) == nil) then
+				if (not getVehicleModelFromName(text)) then
 					exports.UCDdx:new("You must specify a valid vehicle name or ID", 255, 0, 0)
 					return
 				end
 			end
-			triggerServerEvent(action_, var[1], var[2], confirm.edit[1]:getText())
-			
-			--[[		
-			if (action_ == "kickPlayer") then
-				triggerServerEvent("aPlayer", localPlayer, varOne, "kick", guiGetText(aInputValue))
-			elseif (action_ == "setHealth") then
-				triggerServerEvent("aPlayer", localPlayer, varOne, "sethealth", guiGetText(aInputValue))
-			elseif (action_ == "setArmor") then
-				triggerServerEvent("aPlayer", localPlayer, varOne, "setarmour", guiGetText(aInputValue))
-			elseif (action_ == "setNick") then
-				triggerServerEvent("aPlayer", localPlayer, varOne, "setnick", guiGetText(aInputValue))
-			elseif (action_ == "shout") then
-				triggerServerEvent("aPlayer", localPlayer, varOne, "shout", guiGetText(aInputValue))
-			elseif (action_ == "setMoney") then
-				triggerServerEvent("aPlayer", localPlayer, varOne, "setmoney", guiGetText(aInputValue))
-			elseif (action_ == "setDimension") then
-				triggerServerEvent("aPlayer", localPlayer, varOne, "setdimension", guiGetText(aInputValue))
-			elseif (action_ == "setCurrentAmmo") then
-				aSetCurrentAmmo(guiGetText(aInputValue))
-			elseif (action_ == "setGameType") then
-				triggerServerEvent("aServer", localPlayer, "setgame", guiGetText(aInputValue))
-			elseif (action_ == "setMapName") then
-				triggerServerEvent("aServer", localPlayer, "setmap", guiGetText(aInputValue))
-			elseif (action_ == "setWelcome") then
-				triggerServerEvent("aServer", localPlayer, "setwelcome", guiGetText(aInputValue))
-			elseif (action_ == "setServerPassword") then
-				triggerServerEvent("aServer", localPlayer, "setpassword", guiGetText(aInputValue))
-			elseif (action_ == "serverShutdown") then
-				triggerServerEvent("aServer", localPlayer, "shutdown", guiGetText(aInputValue))
-			elseif (action_ == "unbanIP") then
-				triggerServerEvent("aBans", localPlayer, "unbanip", guiGetText(aInputValue))
-			elseif (action_ == "unbanSerial") then
-				triggerServerEvent("aBans", localPlayer, "unbanserial", guiGetText(aInputValue))
-			elseif (action_ == "banIP") then
-				triggerServerEvent("aBans", localPlayer, "banip", guiGetText(aInputValue))
-			elseif (action_ == "banSerial") then
-				triggerServerEvent("aBans", localPlayer, "banserial", guiGetText(aInputValue))
-			elseif (action_ == "settingChange") then
-				triggerServerEvent("aAdmin", localPlayer, "settings", "change", varOne, varTwo, guiGetText(aInputValue))
-			elseif (action_ == "aclCreateGroup") then
-				triggerServerEvent("aAdmin", localPlayer, "aclcreate", "group", guiGetText(aInputValue))
-			elseif (action_ == "aclCreate") then
-				triggerServerEvent("aAdmin", localPlayer, "aclcreate", "acl", guiGetText(aInputValue))
-			elseif (action_ == "aclAddObject") then
-				triggerServerEvent("aAdmin", localPlayer, "acladd", "object", varOne, guiGetText(aInputValue))
-			elseif (action_ == "aclAddRight") then
-				triggerServerEvent("aAdmin", localPlayer, "acladd", "right", varOne, guiGetText(aInputValue))
-			end
-			-]]
-			
+			triggerServerEvent(action_, var[1], var[2], text)
 			action_ = nil
 			closeInputBox()
 		elseif (source == confirm.button[2]) then
