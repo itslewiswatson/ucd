a525b1a62d515348190acaefc440f0b7d0d93589
nokizorque <lewis.watson@outlook.com>
2015-12-14 11:25:36 +1000

    UCDmods
    - Spacing and meta file changes.

--- UCDmods/mods.lua
+++ UCDmods/mods.lua
@@ -5,9 +5,10 @@ function applyMods()
 	local staff = engineLoadTXD( "staff.txd", true )
 	engineImportTXD(staff, 217)
 	local staff = engineLoadDFF( "staff.dff", 217 )
-	engineReplaceModel( staff, 217 )
+	engineReplaceModel(staff, 217)
 	
 	engineImportTXD(engineLoadTXD("infernus.txd", true), 411)
 	engineReplaceModel(engineLoadDFF("infernus.dff", 411), 411)
+		
 end
 addEventHandler( "onClientResourceStart", resourceRoot, applyMods )
