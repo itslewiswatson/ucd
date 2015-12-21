d8c3edb473803918d40fd16f1695d84045b9ff65
nokizorque <lewis.watson@outlook.com>
2015-12-21 16:29:29 +1000

    UCDdata
    - Removed the dollar sign from money.

--- UCDdata/c_scoreboard.lua
+++ UCDdata/c_scoreboard.lua
@@ -1,6 +1,6 @@
 function displayData()
 	if (not exports.UCDaccounts:isPlayerLoggedIn(localPlayer)) then return end
-	localPlayer:setData("dxscoreboard_money", "$"..exports.UCDutil:tocomma(getPlayerMoney(localPlayer)), true)
+	localPlayer:setData("dxscoreboard_money", exports.UCDutil:tocomma(getPlayerMoney(localPlayer)), true)
 	localPlayer:setData("dxscoreboard_city", exports.UCDutil:getPlayerCityZone(localPlayer), true)
 	--localPlayer:setData("dxscoreboard_playtime", localPlayer:getData("playtime"), true) -- Set in UCDplaytime
 end
