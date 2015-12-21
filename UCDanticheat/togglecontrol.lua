124bdd49bfcac6ce6cc6f8e4181ceac2fd313cdb
nokizorque <lewis.watson@outlook.com>
2015-12-21 16:29:59 +1000

    UCDanticheat
    - You can no longer shoot without aiming.

--- UCDanticheat/togglecontrol.lua
+++ UCDanticheat/togglecontrol.lua
@@ -4,3 +4,28 @@ addCommandHandler("tog",
 		toggleControl("fire", not isControlEnabled("fire")) 
 	end
 )
+
+local fireKeys = getBoundKeys("fire")
+local exceptedWeapons = {[41] = true}
+local exceptedSlots = {[0] = true, [1] = true, [8] = true, [10] = true, [11] = true, [12] = true}
+
+function aimCheck(button, state)
+	if (localPlayer.vehicle) then return end
+	if (fireKeys[button] and state == true) then
+		if (exceptedSlots[localPlayer.weaponSlot] or exceptedWeapons[localPlayer:getWeapon()]) then
+			return
+		end
+		outputDebugString("fire")
+		if (getControlState("aim_weapon")) then
+			outputDebugString("is aiming")
+		else
+			toggleControl("fire", false)
+			outputDebugString("not aiming")
+		end
+	end
+	if (fireKeys[button] and state == false) then
+		if (exports.UCDsafeZones:isElementWithinSafeZone(localPlayer) or localPlayer.frozen) then return end
+		toggleControl("fire", true)
+	end
+end
+addEventHandler("onClientKey", root, aimCheck)
