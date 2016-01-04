ada250d599424ac3c523219b245e9c56be5b6e1b
nokizorque <noki@zorque.xyz>
2016-01-04 21:04:04 +1000

    lol
    - Removed Shamal and water shaders.
    - Fixed test colour picker working with multiple colour types.

--- [shaders]/lol/c_tex_names.lua
+++ [shaders]/lol/c_tex_names.lua
@@ -1,39 +1,13 @@
 addEventHandler("onClientResourceStart", resourceRoot,
-	function ()
-		shamalShader = dxCreateShader('tex_names.fx', 0, 0, true, 'vehicle')
-		engineApplyShaderToWorldTexture(shamalShader, 'shamalbody256')
-		
+	function ()		
 		wires = dxCreateShader('tex_names.fx', 0, 0, false)
 		engineApplyShaderToWorldTexture(wires, 'telewires_law')
 		engineApplyShaderToWorldTexture(wires, 'telewireslong')
 		engineApplyShaderToWorldTexture(wires, 'telewireslong2')
 		engineApplyShaderToWorldTexture(wires, 'antenna1')
-		
 		dxSetShaderValue(wires, 'gColor', 255, 0, 0, 0)
-		
-		dd = dxCreateShader('tex_names.fx', 0, 0, false)
-		engineApplyShaderToWorldTexture(dd, 'waterclear256')
-		dxSetShaderValue(dd, 'gColor', 255, 255, 0, 255)
 	end
 )
-	
-function update(r, g, b, a, sync)
-	--dxSetShaderValue(shamalShader, 'gColor', r / 255, g / 255, b / 255, a / 255)
-	dxSetShaderValue(shamalShader, 'gColor', r, g, b, a)
-	outputDebugString("Set shamal to "..r.." "..g.." "..b.." "..a)
-	if sync ~= "a" then
-		triggerServerEvent("onUpdateShamalRGB", localPlayer, r, g, b)
-	end
-end
-
-function updatergb(cmd, nr, ng, nb, na, sync)
-	if (nr and ng and nb and na) then
-		local nr, ng, nb = tonumber(nr), tonumber(ng), tonumber(nb), tonumber(na)
-		update(nr, ng, nb, na, sync)
-	end
-end
-addCommandHandler("rgb", updatergb)
-
 
 addCommandHandler("cp", 
 	function () 
@@ -47,30 +21,20 @@ addCommandHandler("cp",
 
 function updateColor()
 	if (not colorPicker.isSelectOpen) then return end
-	local r, g, b, a = colorPicker.updateTempColors()
-	outputDebugString(a)
-	if (localPlayer.vehicle) then
-		setVehicleColor(localPlayer.vehicle, r, g, b)
-	end
-	--update(r, g, b, a, "a")
-	--dxSetShaderValue(shamalShader, 'gColor', r / 255, g / 255, b / 255, a / 255)
---	if (moddingVeh and isElement(moddingVeh)) then
-	--	local r1, g1, b1, r2, g2, b2 = getVehicleColor(moddingVeh, true)
-	--	if (guiCheckBoxGetSelected(checkColor1)) then
-	--		r1, g1, b1 = r, g, b
-	--	end
-	--	if (guiCheckBoxGetSelected(checkColor2)) then
-	--		r2, g2, b2 = r, g, b
-	--	end
+	local r, g, b = colorPicker.updateTempColors()
+	if (localPlayer.vehicle and isElement(localPlayer.vehicle)) then
+		local r1, g1, b1, r2, g2, b2 = getVehicleColor(localPlayer.vehicle, true)
+		if (guiCheckBoxGetSelected(checkColor1)) then
+			r1, g1, b1 = r, g, b
+		end
+		if (guiCheckBoxGetSelected(checkColor2)) then
+			r2, g2, b2 = r, g, b
+		end
 		--if (guiCheckBoxGetSelected(checkColor3)) then
-		--	setVehicleHeadLightColor(moddingVeh, r, g, b)
+		--	setVehicleHeadLightColor(localPlayer.vehicle, r, g, b)
 		--end
-	--	setVehicleColor(moddingVeh, r1, g1, b1, r2, g2, b2)
-	--	tempColors = {r1, g1, b1, r2, g2, b2}
-	--end
+		setVehicleColor(localPlayer.vehicle, r1, g1, b1, r2, g2, b2)
+		tempColors = {r1, g1, b1, r2, g2, b2}
+	end
 end
 addEventHandler("onClientRender", root, updateColor)
-
-addEvent("onSyncShamalRGB", true)
-addEventHandler("onSyncShamalRGB", root, function (r, g, b) executeCommandHandler("rgb", r.." "..g.." "..b.." a") end)
-
