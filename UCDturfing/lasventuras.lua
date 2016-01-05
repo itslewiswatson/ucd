fe3ef2b67b25629511ad7bb405ec5e33ec4d148f
nokizorque <noki@zorque.xyz>
2016-01-05 19:07:08 +1000

    UCDturfing
    - Removed some LV radar areas and fixed isElementInLV returning a string value.

--- UCDturfing/lasventuras.lua
+++ UCDturfing/lasventuras.lua
@@ -1,12 +1,6 @@
 lv_main = createColRectangle(875.2869, 620.903, 2300, 2400)
-createRadarArea(875.2869, 620.903, 2300, 2400, 255, 255, 255, 100)
-
 lv_bottom = createColRectangle(1334.7622, 545.7134, 1834.5247, 90)
-createRadarArea(1334.7622, 545.7134, 1834.5247, 90, 127, 127, 127, 100)
-
 lv_south_pier = createColRectangle(2218.9824, 487.9441, 230, 100)
-createRadarArea(2218.9824, 487.9441, 230, 100, 127, 127, 127, 100)
-
 lv = {lv_main, lv_bottom, lv_south_pier}
 
 function isElementInLV(ele)
@@ -16,5 +10,5 @@ function isElementInLV(ele)
 			return true
 		end
 	end
-	return "nyes"
+	return false
 end
