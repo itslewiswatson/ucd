0f2617fbc7afcab399284da1e4881a6dc7da8490
nokizorque <noki@zorque.xyz>
2016-01-05 19:04:12 +1000

    UCDanticheat
    - Fixed fireCheck allowing nades to be thrown in safe zones.
    - Added an aim check, so if you're in a team that's not allowed to shoot (unless in LV) or you're in a safezone, you can't shoot or aim.
    - Fixed some other syntax and formatting errors.

--- UCDanticheat/togglecontrol.lua
+++ UCDanticheat/togglecontrol.lua
@@ -5,27 +5,49 @@ addCommandHandler("tog",
 	end
 )
 
+local aimKeys = getBoundKeys("aim_weapon")
 local fireKeys = getBoundKeys("fire")
 local exceptedWeapons = {[41] = true}
 local exceptedSlots = {[0] = true, [1] = true, [8] = true, [10] = true, [11] = true, [12] = true}
+local disallowedTeams = {["Citizens"] = true, ["Not logged in"] = true}
 
-function aimCheck(button, state)
+function fireCheck(button, state)
 	if (localPlayer.vehicle) then return end
 	if (fireKeys[button] and state == true) then
+		if (exports.UCDsafeZones:isElementWithinSafeZone(localPlayer)) then
+			toggleControl("fire", false)
+			return
+		end
 		if (exceptedSlots[localPlayer.weaponSlot] or exceptedWeapons[localPlayer:getWeapon()]) then
 			return
 		end
 		--outputDebugString("fire")
-		if (getControlState("aim_weapon")) then
-			--outputDebugString("is aiming")
-		else
+		if (not getControlState("aim_weapon")) then
 			toggleControl("fire", false)
 			--outputDebugString("not aiming")
 		end
 	end
 	if (fireKeys[button] and state == false) then
-		if (exports.UCDsafeZones:isElementWithinSafeZone(localPlayer) or localPlayer.frozen) then return end
-		toggleControl("fire", true)
+		if (not exports.UCDsafeZones:isElementWithinSafeZone(localPlayer) and not localPlayer.frozen) then
+			toggleControl("fire", true)
+		end
+	end
+end
+addEventHandler("onClientKey", root, fireCheck)
+
+function aimCheck(button, state)
+	if (localPlayer.vehicle) then return end
+	if (aimKeys[button] and state == true) then
+		--if ((disallowedTeams[localPlayer.team.name] and not exports.UCDturfing:isElementInLV(localPlayer)) and localPlayer.weaponSlot ~= 11 and not exceptedWeapons[localPlayer:getWeapon()] and not exports.UCDsafeZones:isElementWithinSafeZone(localPlayer)) then
+		if ((disallowedTeams[localPlayer.team.name] and not exports.UCDturfing:isElementInLV(localPlayer)) or exports.UCDsafeZones:isElementWithinSafeZone(localPlayer)) then
+			outputDebugString("true")
+			toggleControl("aim_weapon", false)
+			toggleControl("fire", false)
+		else
+			outputDebugString("false")
+			toggleControl("aim_weapon", true)
+			toggleControl("fire", true)
+		end
 	end
 end
 addEventHandler("onClientKey", root, aimCheck)
