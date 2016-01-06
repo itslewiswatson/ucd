04665ed10198fb718d8ff7ee13841d73fa7a4b4c
nokizorque <noki@zorque.xyz>
2016-01-06 11:25:21 +1000

    UCDclick
    - Removed redundant/shitty code and added some more shit.

--- UCDclick/client.lua
+++ UCDclick/client.lua
@@ -1,38 +1,23 @@
+nX, nY = guiGetScreenSize()
 isCursor = false
 selectedElement = nil
 
 -- GUI
-local UCD = {window = {}, button = {}}
-UCD.window[1] = guiCreateWindow(574, 226, 270, 281, "UCD | ", false)
-guiWindowSetSizable(UCD.window[1], false)
-guiSetVisible(UCD.window[1], false)
---[[
-UCD.button[1] = guiCreateButton(23, 37, 220, 55, "Destroy vehicle", false, UCD.window[1])
-guiSetProperty(UCD.button[1], "NormalTextColour", "FFAAAAAA")
-UCD.button[2] = guiCreateButton(23, 110, 220, 55, "Warp into vehicle", false, UCD.window[1])
-guiSetProperty(UCD.button[2], "NormalTextColour", "FFAAAAAA")
-UCD.button[3] = guiCreateButton(23, 189, 220, 55, "Find vehicle owner //TBA", false, UCD.window[1])
-guiSetProperty(UCD.button[3], "NormalTextColour", "FFAAAAAA")
+local UCDclick = {window = {}, button = {}}
 
-addEventHandler("onClientGUIClick", guiRoot, destroyVehicle)
-addEventHandler("onClientGUIClick", guiRoot, warpIntoVehicle)
-addEventHandler("onClientGUIClick", guiRoot, findVehicleOwner)
-]]
+UCDclick.window[1] = guiCreateWindow(722, 359, 457, 407, "UCD | Options", false)
+UCDclick.window[1].sizeable = false
+UCDclick.window[1].visible = false
+-- 1st row
+UCDclick.button[1] = guiCreateButton(10, 27, 139, 21, "", false, UCDclick.window[1])
+UCDclick.button[3] = guiCreateButton(159, 27, 139, 21, "", false, UCDclick.window[1])
+UCDclick.button[4] = guiCreateButton(308, 27, 139, 21, "", false, UCDclick.window[1])
+-- 2nd row
+UCDclick.button[5] = guiCreateButton(10, 58, 139, 21, "", false, UCDclick.window[1])
+UCDclick.button[6] = guiCreateButton(159, 58, 139, 21, "", false, UCDclick.window[1])
+UCDclick.button[7] = guiCreateButton(308, 58, 139, 21, "", false, UCDclick.window[1])
+close_ = guiCreateButton(10, 371, 437, 26, "Close", false, UCDclick.window[1])
 
-function playerClick(button, state, _, _, worldX, worldY, worldZ, element)
-	if (button == "left") then
-		if (state == "down") then
-			if (element) then
-				if (getElementType(element) == "player") then
-					outputDebugString("Clicked on player")
-				elseif (getElementType(element) == "vehicle") then
-					outputDebugString("Clicked on vehicle")
-				end
-				selectedElement = element
-			end
-		end
-	end
-end
 
 function _showCursor()
 	if (isCursor) then
@@ -48,38 +33,48 @@ end
 addCommandHandler("cursor", _showCursor)
 bindKey("x", "down", "cursor")
 
+function playerClick(button, state, _, _, worldX, worldY, worldZ, element)
+	if (button == "left" and state == "down") then
+		if (element) then
+			if (getElementType(element) == "player") then
+				outputDebugString("Clicked on player")
+			elseif (getElementType(element) == "vehicle") then
+				if (element:getOccupant(0)) then
+					outputDebugString("Clicked on player in vehicle")
+				else
+					outputDebugString("Clicked on vehicle")
+				end
+			end
+			selectedElement = element
+		end
+	end
+end
+
 function renderCursorOptions()
 	if (not isCursor) then return end
 	local sX, sY, wX, wY, wZ = getCursorPosition()
 	local oX, oY, oZ = getCameraMatrix()
 	local hit, hitX, hitY, hitZ, hitElement = processLineOfSight(oX, oY, oZ, wX, wY, wZ)
 	if (hit and hitElement and isElement(hitElement)) then
-		if (getElementType(hitElement) == "player") then
-			local plrName = getPlayerName(hitElement)
-			dxDrawText("Get options of "..plrName, 527, 285, 839, 328, tocolor(255, 255, 255, 255), 1, "bankgothic", "center", "center")
+		if (hitElement.type == "player") then 
+			drawText("Get options of "..hitElement.name)
 			return
-		elseif (getElementType(hitElement) == "vehicle") then
-			local vehicleName = getVehicleName(hitElement)
-			dxDrawText("Get options of "..vehicleName, 527, 285, 839, 328, tocolor(255, 255, 255, 255), 1, "bankgothic", "center", "center")
+		elseif (hitElement.type == "vehicle") then
+			if (hitElement:getOccupant(0)) then
+				drawText("Get options of "..hitElement:getOccupant(0).name)
+			else
+				drawText("Get options of this "..hitElement.name)
+			end
+			return
+		elseif (hitElement.type == "object") then
+			drawText("No options currently available")
 			return
 		end
 	end
-	dxDrawText("Click on a vehicle for more options", 527, 285, 839, 328, tocolor(255, 255, 255, 255), 1, "bankgothic", "center", "center")
+	drawText("Click on a yourself, a player, an object or a vehicle for options")
 end
 addEventHandler("onClientRender", root, renderCursorOptions)
 
-function destroyVehicle()
-	if (source == UCDdveh) then
-		destroyElement()
-	end
-end
-function warpIntoVehicle()
-	
-end
-function findVehicleOwner()
-	-- To be made
-end
-
-function createUI()
-
+function drawText(text)
+	dxDrawText(tostring(text), 0, 203, nX, nY / 4, tocolor(255, 255, 255, 255), 2, "default", "center", "center", false, false, false, false, false)
 end
