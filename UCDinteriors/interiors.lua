043d448c632a2368916385216d55c87f44bb543c
nokizorque <noki@zorque.xyz>
2016-01-06 11:26:59 +1000

    UCDinteriors
    - Added paramedic interiors.
    - Linked resource UCDjobsTable.

--- UCDinteriors/interiors.lua
+++ UCDinteriors/interiors.lua
@@ -20,5 +20,15 @@ interiors =
 		{entryX = -2033, entryY = 161.4235, entryZ = 29.1, exitX = -2040, exitY = 155.13, exitZ = 28.9, dimension = 50001, interior = 1}, -- San Fierro
 		{entryX = 1965.0718, entryY = 2141.8677, entryZ = 10.85, exitX = -2040, exitY = 155.13, exitZ = 28.9, dimension = 50002, interior = 1}, -- Las Venturas
 		{entryX = -2089, entryY = -2262.5, entryZ = 30.7, exitX = -2040, exitY = 155.13, exitZ = 28.9, dimension = 50003, interior = 1}, -- Angel Pine
-	}
+	},
+	["Paramedic"] = 
+	{
+		{entryX = 2034.0787, entryY = -1401.9, entryZ = 17.3, exitX = 238.6942, exitY = 138.8, exitZ = 1003.1, dimension = 50000, interior = 3}, -- Jefferson
+		{entryX = 1172.0767, entryY = -1323.3193, entryZ = 15.42, exitX = 238.6942, exitY = 138.8, exitZ = 1003.1, dimension = 50001, interior = 3}, -- All Saints
+		{entryX = -2655.114, entryY = 640.1652, entryZ = 14.5, exitX = 238.6942, exitY = 138.8, exitZ = 1003.1, dimension = 50002, interior = 3}, -- San Fierro
+		{entryX = -320.1846, entryY = 1048.2339, entryZ = 20.3403, exitX = 238.6942, exitY = 138.8, exitZ = 1003.1, dimension = 50003, interior = 3}, -- Fort Carson
+		{entryX = 1607.4338, entryY = 1815.2468, entryZ = 10.9, exitX = 238.6942, exitY = 138.8, exitZ = 1003.1, dimension = 50004, interior = 3}, -- Las Venturas
+		{entryX = -1514.8516, entryY = 2518.9285, entryZ = 56.1, exitX = 238.6942, exitY = 138.8, exitZ = 1003.1, dimension = 50005, interior = 3}, -- El Quebrados
+		{entryX = 1241.3721, entryY = 325.8875, entryZ = 19.78, exitX = 238.6942, exitY = 138.8, exitZ = 1003.1, dimension = 50006, interior = 3}, -- Crippen Memorial
+	},
 }
