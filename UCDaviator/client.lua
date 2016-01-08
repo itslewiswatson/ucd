a21a12761bbc4cce55917e8b87d306e8c389b072
nokizorque <noki@zorque.xyz>
2016-01-08 22:48:37 +1000

    UCDaviator
    - Added settings.
    - Moved the table to a diffetent file and made it shared.
    - Big planes can no longer get a flight to VM.
    - Revamped the itinerary system.
    - Fixed zones defaulting to city because of Z axis.
    - Fixed markers creating themselves too high.
    - Added a method to slow helicopters when they hit the marker.
    - Added a simple switch to check whether the player has already hit a marker to avoid double hit detections.
    - Added server flight processing which calculates bonuses etc.
    - Added some utility functions from GTI.

--- UCDaviator/client.lua
+++ UCDaviator/client.lua
@@ -1,50 +1,35 @@
+
+-- Settings
+local MARKER_SIZE = 8 -- Size of the markers
+local HELI_MARKER_DIST = 10 -- Max Z distance of a marker hit registering for a helicopter
+local WAIT_TIME = 2000
+
+-- Utility tables
 local cargoPlanes = {[592] = true, [511] = true, [593] = true, [513] = true}
 local passgenerPlanes = {[577] = true, [553] = true, [519] = true}
-local airports = {[1] = "Los Santos", [2] = "San Fierro", [3] = "Las Venturas", [4] = "Verdant Meadows"}
-local destinations = 
-{
-	["Plane"] = 
-	{
-		["Los Santos"] = 
-		{
-			{0, 0, 0},
-			{0, 0, 0},
-		},
-		["San Fierro"] = 
-		{
-			{-1289.8391, -1.7542, 14.1484},
-		},
-		["Las Venturas"] = 
-		{
-			{1335.9799, 1588.3812, 10.8203},
-			{0, 0, 0},
-		},
-		["Verdant Meadows"] = 
-		{
-			{358.5139, 2538.1794, 16.6982},
-			{0, 0, 0},
-		},
-	},
-	["Helicopter"] = 
-	{
-		{1544.1847, -1353.4109, 329.4742},
-		{1451.7689, -1066.7189, 213.3828},
-		{1765.832, -2286.3018, 26.796},
-		{2761.9385, -1454.833, 66.8605},
-	},
-}
+local sml = {[511] = true, [593] = true, [513] = true} -- Beagle, Dodo, Stuntplane
+local lrg = {[592] = true, [577] = true, [553] = true} -- Nevada, AT-400, Andromada
+local mid = {[519] = true} -- Shamal
+
+-- Other tables and settings used throughout that relate to the flight(s)
 local curr = {} -- Contains the current flight itinerary
 local prev = {} -- Contains the previous flight itinerary
 local markings = {}
 local pending
+local hasHit
 
-addEventHandler("onClientPlayerVehicleEnter", root,
-	function (vehicle, seat)
-		if (source == localPlayer and seat == 0 and (vehicle.vehicleType == "Plane" or vehicle.vehicleType == "Helicopter")) then
-			pending = Timer(calculateItinerary, 750, 1, vehicle.vehicleType)
+function startItinerary(vehicle, seat)
+	if (source == localPlayer and localPlayer.team.name == "Citizens" and localPlayer:getData("Occupation") == "Aviator" and seat == 0 and (vehicle.vehicleType == "Plane" or vehicle.vehicleType == "Helicopter")) then
+		if ((not cargoPlanes[localPlayer.vehicle.model] and not passgenerPlanes[localPlayer.vehicle.model]) and localPlayer.vehicle.vehicleType ~= "Helicopter") then
+			outputDebugString("Not right type of vehicle")
+			return
 		end
+		pending = Timer(calculateItinerary, 750, 1)
 	end
-)
+end
+addEventHandler("onClientPlayerVehicleEnter", root, startItinerary)
+addEvent("UCDaviator.startItinerary", true)
+addEventHandler("UCDaviator.startItinerary", root, startItinerary)
 
 function cancelItinerary()
 	if (isItinerary()) then
@@ -61,12 +46,13 @@ function cancelItinerary()
 		markings.blip:destroy()
 		markings.marker = nil
 		markings.blip = nil
+		hasHit = nil
 	end
 end
 addEventHandler("onClientPlayerWasted", localPlayer, cancelItinerary)
 addEventHandler("onClientElementDestroy", root, function () if (localPlayer.vehicle and source == localPlayer.vehicle) then cancelItinerary() end end)
 addEventHandler("onClientPlayerVehicleExit", localPlayer, cancelItinerary)
-addEvent("onClientPlayerGetJob")
+addEvent("onClientPlayerGetJob", true)
 addEventHandler("onClientPlayerGetJob", localPlayer, cancelItinerary)
 
 function isItinerary()
@@ -76,17 +62,29 @@ function isItinerary()
 	return false
 end
 
-function calculateItinerary(vehType)
-	if (vehType == "Plane") then
+function calculateItinerary()
+	if (not localPlayer.vehicle) then
+		return
+	end
+	if (localPlayer.vehicle.vehicleType == "Plane") then
 		
 		if (curr.airport) then
 			prev.airport = curr.airport
+			prev.dest = curr.dest
 		end
 		
-		repeat 
-			curr.airport = math.random(1, #airports)
-			curr.dest = math.random(1, #destinations["Plane"][airports[curr.airport]])
-		until curr.airport ~= prev.airport
+		-- We don't want the big planes in VM
+		if (not lrg[localPlayer.vehicle.model]) then
+			repeat 
+				curr.airport = math.random(1, #airports)
+				curr.dest = math.random(1, #destinations["Plane"][airports[curr.airport]])
+			until curr.airport ~= prev.airport
+		else
+			repeat 
+				curr.airport = math.random(1, #airports)
+				curr.dest = math.random(1, #destinations["Plane"][airports[curr.airport]])
+			until curr.airport ~= prev.airport and curr.airport ~= 4
+		end
 		
 		if (prev.airport) then
 			markings.marker:destroy()
@@ -95,27 +93,27 @@ function calculateItinerary(vehType)
 			markings.blip = nil
 			
 			local dest = destinations["Plane"][airports[curr.airport]][curr.dest]
-			markings.marker = Marker(dest[1], dest[2], dest[3], "cylinder", 3, 255, 255, 0, 120)
+			markings.marker = Marker(dest[1], dest[2], dest[3] - 1, "cylinder", MARKER_SIZE, 255, 215, 0, 120)
 			markings.blip = Blip.createAttachedTo(markings.marker, 41, nil, nil, nil, nil, nil, 0, 65535)
 			
 			if (cargoPlanes[localPlayer.vehicle.model]) then
-				exports.UCDdx:new("ATC: Take this cargo to "..airports[curr.airport], 255, 255, 0)
+				exports.UCDdx:new("ATC: Take this cargo to "..airports[curr.airport], 255, 215, 0)
 			elseif (passgenerPlanes[localPlayer.vehicle.model]) then
-				exports.UCDdx:new("ATC: Take these passgeners to "..airports[curr.airport], 255, 255, 0)
+				exports.UCDdx:new("ATC: Take these passgeners to "..airports[curr.airport], 255, 215, 0)
 			end
 		else
 			local dest = destinations["Plane"][airports[curr.airport]][curr.dest]
-			markings.marker = Marker(dest[1], dest[2], dest[3], "cylinder", 3, 255, 255, 0, 120)
+			markings.marker = Marker(dest[1], dest[2], dest[3] - 1, "cylinder", MARKER_SIZE, 255, 215, 0, 120)
 			markings.blip = Blip.createAttachedTo(markings.marker, 41, nil, nil, nil, nil, nil, 0, 65535)
 			
 			if (cargoPlanes[localPlayer.vehicle.model]) then
-				exports.UCDdx:new("ATC: There has been a request for cargo pickup at "..airports[curr.airport], 255, 255, 0)
+				exports.UCDdx:new("ATC: There has been a request for cargo pickup at "..airports[curr.airport], 255, 215, 0)
 			elseif (passgenerPlanes[localPlayer.vehicle.model]) then
-				exports.UCDdx:new("ATC: There has been a request for passgener pickup at "..airports[curr.airport], 255, 255, 0)
+				exports.UCDdx:new("ATC: There has been a request for passgener pickup at "..airports[curr.airport], 255, 215, 0)
 			end
 		end
 		
-	elseif (vehType == "Helicopter") then
+	elseif (localPlayer.vehicle.vehicleType == "Helicopter") then
 		
 		if (curr.dest) then
 			prev.dest = curr.dest
@@ -133,16 +131,26 @@ function calculateItinerary(vehType)
 			markings.blip = nil
 
 			local dest = destinations["Helicopter"][curr.dest]
-			markings.marker = Marker(dest[1], dest[2], dest[3], "cylinder", 3, 255, 255, 0, 120)
+			markings.marker = Marker(dest[1], dest[2], dest[3] - 1, "cylinder", MARKER_SIZE, 255, 215, 0, 120)
 			markings.blip = Blip.createAttachedTo(markings.marker, 41, nil, nil, nil, nil, nil, 0, 65535)
 			
-			exports.UCDdx:new("PASSENGER: Take me to "..getZoneName(dest[1], dest[2], dest[3]).." in "..getZoneName(dest[1], dest[2], dest[3], true)..", please!", 255, 255, 0)
+			local suburb, city = getZoneName(dest[1], dest[2], 0), getZoneName(dest[1], dest[2], 0, true)
+			if (suburb ~= city) then
+				exports.UCDdx:new("PASSENGER: Take me to "..suburb.." in "..city..", please!", 255, 215, 0)
+			else
+				exports.UCDdx:new("PASSENGER: Take me to "..suburb..", please!", 255, 215, 0)
+			end
 		else
 			local dest = destinations["Helicopter"][curr.dest]
-			markings.marker = Marker(dest[1], dest[2], dest[3], "cylinder", 3, 255, 255, 0, 120)
+			markings.marker = Marker(dest[1], dest[2], dest[3] - 1, "cylinder", MARKER_SIZE, 255, 215, 0, 120)
 			markings.blip = Blip.createAttachedTo(markings.marker, 41, nil, nil, nil, nil, nil, 0, 65535)
 			
-			exports.UCDdx:new("ATC: A private passgener is requesting pickup. Make your way to "..getZoneName(dest[1], dest[2], dest[3])..", "..getZoneName(dest[1], dest[2], dest[3], true)..".", 255, 255, 0)
+			local suburb, city = getZoneName(dest[1], dest[2], 0), getZoneName(dest[1], dest[2], 0, true)
+			if (suburb ~= city) then
+				exports.UCDdx:new("ATC: A private passgener is requesting pickup. Make your way to "..suburb..", "..city..".", 255, 215, 0)
+			else
+				exports.UCDdx:new("ATC: A private passgener is requesting pickup. Make your way to "..suburb..".", 255, 215, 0)
+			end
 		end
 	end
 	
@@ -151,20 +159,76 @@ end
 
 function onClientMarkerHit(plr, matchingDimension)
 	if (plr == localPlayer and matchingDimension) then
-		if (localPlayer.vehicle.vehicleType == "Helicopter") then
-			if (prev.dest) then
-				exports.UCDdx:new("You completed this flight", 255, 255, 0)
-				cancelItinerary()
+		
+		if (hasHit) then
+			return
+		end
+		
+		outputDebugString("H: "..localPlayer.vehicle.position.z)
+		outputDebugString("M: "..source.position.z)
+		if (localPlayer.vehicle.vehicleType == "Helicopter" and localPlayer.vehicle.position.z - HELI_MARKER_DIST > source.position.z) then
+			return
+		end
+		
+		--stopVehicle(true)
+		
+		if (prev.airport or prev.dest) then
+			if (localPlayer.vehicle.vehicleType == "Plane") then
+				if (cargoPlanes[localPlayer.vehicle.model]) then
+					exports.UCDdx:new("ATC: Please wait while your aircraft is unloaded", 255, 215, 0)
+				else
+					exports.UCDdx:new("ATC: Please wait while your passgeners disembark the aircraft", 255, 215, 0)
+				end
 			else
-				calculateItinerary("Helicopter")
+				exports.UCDdx:new("ATC: Please wait while the private passgener disembarks", 255, 215, 0)
 			end
-		elseif (localPlayer.vehicle.vehicleType == "Plane") then
+		else
+			if (localPlayer.vehicle.vehicleType == "Plane") then
+				if (cargoPlanes[localPlayer.vehicle.model]) then
+					exports.UCDdx:new("ATC: Please wait while your aircraft is loaded", 255, 215, 0)
+				else
+					exports.UCDdx:new("ATC: Please wait while your passgeners embark the aircraft", 255, 215, 0)
+				end
+			else
+				exports.UCDdx:new("ATC: Please wait while the private passgener embarks", 255, 215, 0)
+			end
+		end
+		
+		
+		--localPlayer.vehicle.rotation = Vector3(0, 0, localPlayer.vehicle.rotation.z)
+		--localPlayer.vehicle.velocity = Vector3(0, 0, -0.1)
+		--localPlayer.vehicle.position = Vector3(localPlayer.vehicle.position.x, localPlayer.vehicle.position.y, source.position.z + 1)
+		--localPlayer.vehicle.frozen = true
+		
+		stopVehicle(true)
+		
+		hasHit = true
+		
+		Timer(onCompleteWaitForPassengers, WAIT_TIME, 1)
+	end
+end
+
+function onCompleteWaitForPassengers()
+	if (localPlayer.vehicle) then
+	
+		stopVehicle()
+		hasHit = nil
+		
+		if (localPlayer.vehicle.vehicleType == "Plane") then
 			if (prev.airport) then
-				exports.UCDdx:new("You completed this flight", 255, 255, 0)
+				triggerServerEvent("UCDaviator.processFlight", localPlayer, {curr.airport, curr.dest, prev.airport, prev.dest}, localPlayer.vehicle)
 				cancelItinerary()
 			else
-				calculateItinerary("Plane")
+				calculateItinerary()
+			end
+		elseif (localPlayer.vehicle.vehicleType == "Helicopter") then
+			if (prev.dest) then
+				triggerServerEvent("UCDaviator.processFlight", localPlayer, {curr.dest, prev.dest}, localPlayer.vehicle)
+				cancelItinerary()
+			else
+				calculateItinerary()
 			end
 		end
 	end
-end	
+end
+
---
+++ UCDaviator/server.lua
@@ -0,0 +1,85 @@
+--[[
+-- Distances
+
+LS --> LV = 4000
+LS --> VM = 5000
+LS --> SF = 3500
+LV --> SF = 3300
+LV --> VM = 1500
+SF --> VM = 3000
+
+--]]
+
+local sml = {[511] = true, [593] = true, [513] = true} -- Beagle, Dodo, Stuntplane
+local lrg = {[592] = true, [577] = true, [553] = true} -- Nevada, AT-400, Andromada
+local mid = {[519] = true} -- Shamal
+
+addEventHandler("onResourceStart", resourceRoot,
+	function ()
+		ranks = exports.UCDjobsTable:getJobRanks("Aviator")
+	end
+)
+
+function processFlight(flightData, vehicle)
+	if (client and flightData and vehicle) then
+		
+		local model = vehicle.model
+		local playerRank = 9 -- Change this in the future
+		local base, bonus, distance
+		local start, finish
+		
+		if (vehicle.vehicleType == "Plane") then
+			start = destinations["Plane"][airports[flightData[3]]][flightData[4]]
+			finish = destinations["Plane"][airports[flightData[1]]][flightData[2]]
+			local x1, y1, z1 = start[1], start[2], start[3]
+			local x2, y2, z2 = finish[1], finish[2], finish[3]
+			distance = getDistanceBetweenPoints3D(x1, y1, z1, x2, y2, z2)
+			--outputDebugString("Distance: "..distance)
+			
+			if (sml[model]) then
+				base = math.random((distance * 1.15) - 250, (distance * 1.15) + 250)
+			elseif (lrg[model]) then
+				base = math.random((distance * 2.5) - 250, (distance * 2.5) + 250)
+			elseif (mid[model]) then
+				base = math.random((distance * 1.75) - 250, (distance * 1.75) + 400)
+			else
+				outputDebugString("Unknown type given")
+				return
+			end
+		else
+			start = destinations["Helicopter"][flightData[1]]
+			finish = destinations["Helicopter"][flightData[2]]
+			local x1, y1, z1 = start[1], start[2], start[3]
+			local x2, y2, z2 = finish[1], finish[2], finish[3]
+			distance = getDistanceBetweenPoints3D(x1, y1, z1, x2, y2, z2)
+			--outputDebugString("Distance: "..distance)
+			
+			-- You should earn slightly more with a helicopter
+			base = math.random(distance * 1.5, (distance * 1.5) + 350)
+		end
+		
+		bonus = ranks[playerRank].bonus
+		
+		local newAmount = math.floor((base * (bonus / 100)) + base)
+		local formattedAmount = exports.UCDutil:tocomma(newAmount)
+		
+		outputDebugString("Base = "..base)
+		outputDebugString("Bonus = "..bonus.."%")
+		outputDebugString("New amount = "..newAmount)
+		
+		if (vehicle.vehicleType == "Plane") then
+			local fA = airports[flightData[1]]
+			local sA = airports[flightData[3]]
+			exports.UCDdx:new(client, "ATC: Flight from "..sA.." to "..fA.." complete. You have been paid $"..formattedAmount..".", 255, 215, 0)
+		else
+			exports.UCDdx:new(client, "ATC: Flight complete. You have been paid $"..formattedAmount..".", 255, 215, 0)
+		end
+		
+		if (client.vehicle) then
+			--client.vehicle.frozen = false
+			triggerClientEvent(client, "UCDaviator.startItinerary", client, client.vehicle, client.vehicleSeat)
+		end
+	end
+end
+addEvent("UCDaviator.processFlight", true)
+addEventHandler("UCDaviator.processFlight", root, processFlight)
