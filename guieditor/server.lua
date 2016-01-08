8b4abbddd0a66b802945347c37856d740b1c584b
nokizorque <noki@zorque.xyz>
2016-01-08 14:18:05 +1000

    guieditor
    - Added hud replica from CIT.
    - Fixed guieditor causing microstutters.

--- guieditor/server.lua
+++ guieditor/server.lua
@@ -24,31 +24,6 @@ local files = {}
 
 local resourceName = getResourceName(getThisResource())
 
-
-addEventHandler("onResourceStart", resourceRoot, 
-	function()
-		if _DEBUG then
-			for _,p in ipairs(getElementsByType("player")) do
-				bindKey(p, "F2", "up", 
-					function() 
-						restartResource(resource)
-					end
-				)
-			end
-			
-			addEventHandler("onPlayerJoin", root,
-				function()
-					bindKey(source, "F2", "up", 
-						function() 
-							restartResource(resource)
-						end
-					)				
-				end
-			)
-		end
-	end
-)
-
 addEvent("guieditor:server_getImages", true)
 addEventHandler("guieditor:server_getImages", root,
 	function()
