da6d7d8134af980e4ad24207bd375e3b482cc1ee
nokizorque <noki@zorque.xyz>
2016-01-09 19:29:14 +1000

    UCDcriminal
    - Added a simple /criminal command.
    - Players automatically become Gangsters upon entering LV if they were Criminals beforehand and vice versa.

--- UCDcriminal/server.lua
+++ UCDcriminal/server.lua
@@ -1,10 +1,35 @@
-local criminalRanks = 
-{
-	[1] = "Delinquent",
-	[2] = "Crook",
-	[3] = "Thug",
-	[4] = "Mobster",
-	[5] = "Mafioso",
-}
+function doCriminal(plr)
+	if (not plr) then
+		return
+	end
+	if (plr.vehicle) then
+		exports.UCDdx:new(plr, "You can't become a criminal while in a vehicle", 255, 0, 0)
+		return
+	end
+	if (exports.UCDteams:isPlayerInTeam(plr, "Criminals")) then
+		return
+	end
+	if (exports.UCDturfing:isElementInLV(plr) and exports.UCDteams:isPlayerInTeam(plr, "Gangsters")) then
+		return
+	end
+	exports.UCDjobs:setPlayerJob(plr, "Criminal")
+end
+addCommandHandler("criminal", doCriminal, false, false)
 
--- We may want to use some sort of SAD method
+function makeGangster()
+	if (exports.UCDteams:isPlayerInTeam(source, "Criminals")) then
+		exports.UCDjobs:setPlayerJob(source, "Gangster")
+	end
+end
+addEvent("onPlayerEnterLV")
+addEventHandler("onPlayerEnterLV", root, makeGangster)
+addEvent("onPlayerTakeJob")
+addEventHandler("onPlayerTakeJob", root, makeGangster)
+
+function removeGangster()
+	if (exports.UCDteams:isPlayerInTeam(source, "Gangsters")) then
+		exports.UCDjobs:setPlayerJob(source, "Criminal")
+	end
+end
+addEvent("onPlayerLeaveLV")
+addEventHandler("onPlayerLeaveLV", root, removeGangster)
