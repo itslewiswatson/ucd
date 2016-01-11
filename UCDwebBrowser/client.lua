9de93f2834ba3c96c1642314cbdc74ae83ff316e
nokizorque <noki@zorque.xyz>
2016-01-11 16:02:42 +1000

    UCDwebBrowser
    - Clicking F3 now opens the browser if it previously wasn't, and destroyes it if open.

--- UCDwebBrowser/client.lua
+++ UCDwebBrowser/client.lua
@@ -1,31 +1,11 @@
 screenWidth, screenHeight = guiGetScreenSize()
 
-addEventHandler("onClientResourceStart", resourceRoot,
-	function()
-		--bindKey("ralt", "down", function(button, state) showCursor(not isCursorShowing()) end)
-		--addEventHandler("onClientRender", root, onDraw)
-		--addEventHandler("onClientClick", root, onClick)
-	end
-)
-
-function onDraw()
-	if isCursorShowing() then
-		local posX, posY = getCursorPosition()
-		posX, posY = posX * screenWidth, posY * screenHeight
-		if (isPointInRect(posX, posY, 0, screenHeight - 48, 48, screenHeight)) then
-			dxDrawRectangle(0, screenHeight - 48, 48, 48, tocolor(255, 255, 255, 110))
-		end
-	end
-	dxDrawImage(0, screenHeight - 48, 48, 48, "chromiumIcon.png")
-end
-
 function onClick()
-	--if not isCursorShowing() then return end
-	--if button == "left" and state == "up" then
-	--	if isPointInRect(posX, posY, 0, screenHeight - 48, 48, screenHeight) then
-			showBrowser()
-	--	end
-	--end
+	if (WebBrowserGUI.instance) then
+		WebBrowserGUI.instance:CloseButton_Click("left", "up")
+	else
+		showBrowser()
+	end
 end
 bindKey("F3", "up", onClick)
 
