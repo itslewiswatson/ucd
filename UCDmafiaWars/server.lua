d97a0529e6bc88c6bfb9ce67bfcbdf44f5679ee1
nokizorque <noki@zorque.xyz>
2016-01-15 20:51:51 +1000

    UCDturfing -> UCDmafiaWars
    - Turfs that need remapping are now red with finished ones being white.
    - Remapped some more turfs.

--- UCDmafiaWars/server.lua
+++ UCDmafiaWars/server.lua
@@ -1,24 +1,26 @@
 local AreaPosition = {
-    [1] = {aPosX = 1382, aPosY = 903, aSizeX = 115, aSizeY = 220, spawn = {1435, 974, 10.8, 0}},
-    [2] = {aPosX = 1577.7, aPosY = 883, aSizeX = 180, aSizeY = 240, spawn = {1632, 972, 10.8, 270}},
+    [1] = {x = 1377.5, y = 903, z = 9, w = 120, l = 220, h = 20, spawn = {1435, 974, 10.8, 0}},
+    [2] = {x = 1577.7, y = 883, z = 9, w = 180, l = 240, h = 20, spawn = {1632, 972, 10.8, 270}},
+	
+	
     [3] = {aPosX = 1873, aPosY = 939, aSizeX = 160, aSizeY = 149, spawn = {2022, 1008, 10.8, 270}},
     [4] = {aPosX = 1837, aPosY = 1103, aSizeX = 195, aSizeY = 160, spawn = {2012, 1167, 10.8, 270}},
     [5] = {aPosX = 2077, aPosY = 982, aSizeX = 260, aSizeY = 205, spawn = {2183, 1115, 12.6, 60}},
     [6] = {aPosX = 2082, aPosY = 1203, aSizeX = 335, aSizeY = 160, spawn = {2233, 1285, 10.8, 90}},
 	
-    [7] = {cX = 1014, cY = 985, cZ = 5, cW = 154.4, cD = 180, cH = 20, spawn = {1044, 1013, 11, 320}},
-    [8] = {cX = 1017, cY = 1202, cZ = 5, cW = 160.3, cD = 161.1, cH = 20, spawn = {1061, 1259, 10.8, 270}}, -- Add CIT style staircase
-    [9] = {cX = 1017, cY = 1383, cZ = 0, cW = 160, cD = 321, cH = 17, spawn = {1098, 1604, 12.5, 0}},
-    [10] = {cX = 918, cY = 1623, cZ = 5, cW = 79.5, cD = 220.3, cH = 12, spawn = {941, 1733, 10.8, 270}},
-	
-    [11] = {aPosX = 917, aPosY = 1963.3, aSizeX = 81, aSizeY = 221.3, spawn = {971, 2133, 10.8, 270}},
-    [12] = {aPosX = 1017.7, aPosY = 1837, aSizeX = 160, aSizeY = 207, spawn = {1072, 1915, 10.8, 0}},
-	
-    [13] = {cX = 1018, cY = 2063.5, cZ = 5, cW = 160, cD = 218, cH = 20, spawn = {1050, 2122, 10.8, 90}},
+    [7] = {x = 1014, y = 985, z = 5, w = 154.4, l = 180, h = 20, spawn = {1044, 1013, 11, 320}},
+    [8] = {x = 1017, y = 1202, z = 5, w = 160.3, l = 161.1, h = 20, spawn = {1061, 1259, 10.8, 270}}, -- Add CIT style staircase
+    [9] = {x = 1017, y = 1383, z = 0, w = 160, l = 321, h = 17, spawn = {1098, 1604, 12.5, 0}},
+    [10] = {x = 918, y = 1623, z = 5, w = 79.5, l = 220.3, h = 12, spawn = {941, 1733, 10.8, 270}},
+    [11] = {x = 917, y = 1963.3, z = 5, w = 81, l = 221.3, h = 20, spawn = {971, 2133, 10.8, 270}},
+    [12] = {x = 1017.7, y = 1837, z = 5, w = 160, l = 207, h = 20, spawn = {1072, 1915, 10.8, 0}},
+    [13] = {x = 1018, y = 2063.5, z = 5, w = 160, l = 218, h = 20, spawn = {1050, 2122, 10.8, 90}},
 	
     [14] = {aPosX = 1398, aPosY = 2323, aSizeX = 161, aSizeY = 61, spawn = {1500, 2366, 10.8, 0}},
     [15] = {aPosX = 1577, aPosY = 2282.5, aSizeX = 180, aSizeY = 130, spawn = {1680, 2321, 10.8, 270}},
-    [16] = {aPosX = 1250, aPosY = 2516, aSizeX = 350, aSizeY = 115, spawn = {1385, 2523, 10.6, 0}},
+	
+    [16] = {x = 1250, y = 2516, z = 9, w = 350, l = 113, h = 20, spawn = {1385, 2523, 10.6, 0}},
+	
     [17] = {aPosX = 1698, aPosY = 2720, aSizeX = 220, aSizeY = 165, spawn = {1765, 2860, 10.8, 180}},
     [18] = {aPosX = 1778, aPosY = 2564, aSizeX = 200, aSizeY = 120, spawn = {1850, 2583, 10.8, 0}},
     [19] = {aPosX = 1833, aPosY = 2282, aSizeX = 95, aSizeY = 190, spawn = {1882, 2339, 11, 270}},
@@ -43,35 +45,37 @@ local AreaPosition = {
     [38] = {aPosX = 2135, aPosY = 1784, aSizeX = 270, aSizeY = 99, spawn = {2220, 1838, 10.8, 90}},
     [39] = {aPosX = 2160, aPosY = 1904, aSizeX = 170, aSizeY = 110, spawn = {2323, 1991, 5.35, 180}},
     [40] = {aPosX = 2160, aPosY = 2033, aSizeX = 170, aSizeY = 170, spawn = {2275, 2039, 10.8, 270}},
-    [41] = {aPosX = 2098, aPosY = 883, aSizeX = 160, aSizeY = 85, spawn = {2239, 963, 10.8, 10}},
-    [42] = {aPosX = 2777, aPosY = 833, aSizeX = 100, aSizeY = 185, spawn = {2845, 983, 10.8, 94}},
-    [43] = {aPosX = 2517, aPosY = 703, aSizeX = 185, aSizeY = 100, spawn = {2597, 758, 11.2, 359}},    
-    [44] = {aPosX = 2163, aPosY = 642, aSizeX = 270, aSizeY = 120, spawn = {2255, 728, 11.2, 265}},
-    [45] = {aPosX = 2000, aPosY = 641, aSizeX = 140, aSizeY = 125, spawn = {2086, 688, 11.2, 179}},
-    [46] = {aPosX = 1857, aPosY = 641, aSizeX = 130, aSizeY = 125, spawn = {1913, 703, 11.3, 357}},
-    [47] = {aPosX = 1577, aPosY = 663, aSizeX = 170, aSizeY = 115, spawn = {1664, 709, 10.8, 1}},
+	
+    [41] = {x = 2097.5, y = 903.5, z = 5, w = 130, l = 65, h = 20, spawn = {2239, 963, 10.8, 10}},
+    [42] = {x = 2777, y = 833, z = 10, w = 118, l = 190.5, h = 20, spawn = {2845, 983, 10.8, 94}},
+    [43] = {x = 2517, y = 703, z = 5, w = 160.5, l = 60, h = 20, spawn = {2597, 758, 11.2, 359}},
+    [44] = {x = 2157.5, y = 643, z = 5, w = 260, l = 120, h = 15, spawn = {2255, 728, 11.2, 265}},
+    [45] = {x = 2001.1, y = 633.3, z = 5, w = 135.4, l = 160, h = 15, spawn = {2086, 688, 11.2, 179}},
+    [46] = {x = 1877.6, y = 644, z = 9, w = 99.9, l = 119, h = 15, spawn = {1913, 703, 11.3, 357}},
+    [47] = {x = 1577, y = 663, z = 5, w = 180.5, l = 120, h = 20, spawn = {1664, 709, 10.8, 1}},
+	
+	
     [48] = {aPosX = 2237, aPosY = 2723, aSizeX = 210, aSizeY = 100, spawn = {2367, 2757, 10.4, 177}},
     [49] = {aPosX = 2524, aPosY = 2645, aSizeX = 210, aSizeY = 200, spawn = {2589, 2790, 10.5, 92}},
     [50] = {aPosX = 2756, aPosY = 2312, aSizeX = 140, aSizeY = 290, spawn = {2846, 2375, 11.3, 94}},
     [51] = {aPosX = 2365, aPosY = 2061, aSizeX = 140, aSizeY = 165, spawn = {2441, 2160, 10.3, 177}},
-    --[52] = {aPosX = 1283, aPosY = 1224, aSizeX = 75, aSizeY = 80, spawn = {2441, 2160, 10.3, 177}},
-    --[53] = {aPosX = 1264, aPosY = 1311, aSizeX = 75, aSizeY = 120, spawn = {2441, 2160, 10.3, 177}},
+
+	
 }
 
-function handleEntry()
-	outputDebugString("Entered turf with turfID = "..source:getData("id"))
+function handleEntry(ele)
+	if (ele and ele.type == "player") then
+		outputDebugString("Entered turf with turfID = "..source:getData("id"))
+	end
 end
-
-lolk = {7, 8, 9, 10, 13}
 
 addEventHandler("onResourceStart", resourceRoot,
 	function ()
 		
 		for k, v in pairs(AreaPosition) do
-			if k == 7 or k == 8 or k == 9 or k == 10 or k == 13 then
-			--if lolk[k] then
-				local TurfArea = createRadarArea(v.cX, v.cY, v.cW, v.cD, 255, 0, 0, 175)
-				local TurfAreaCol = createColCuboid(v.cX, v.cY, v.cZ, v.cW, v.cD, v.cH)
+			if v.x then
+				local TurfArea = createRadarArea(v.x, v.y, v.w, v.l, 255, 255, 255, 175)
+				local TurfAreaCol = createColCuboid(v.x, v.y, v.z, v.w, v.l, v.h)
 				TurfAreaCol:setData("id", k)
 				addEventHandler("onColShapeHit", TurfAreaCol, handleEntry)
 			else
