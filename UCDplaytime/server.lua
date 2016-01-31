c8c1d901233f0cb9cc6deed270d12f79f28e387f
Carl Rizk <carlrizk88@gmail.com>
2016-01-31 12:36:17 +0200

    UCDPlaytime

--- UCDplaytime/server.lua
+++ UCDplaytime/server.lua
@@ -5,7 +5,7 @@ local playerTickCount = {}
 function onLogin(_, theCurrentAccount)
 	if (not isGuestAccount(theCurrentAccount)) then
 		source:setData("playtime", exports.UCDaccounts:GAD(source, "playtime"), true)
-		playerTickCount[source] = getTickCount()
+		playerTickCount[source] = getRealTime().timestamp
 	end
 end
 addEventHandler("onPlayerLogin", root, onLogin)
@@ -16,7 +16,7 @@ function onQuit()
 	end
 	
 	local pAccTime  = exports.UCDaccounts:GAD(source, "playtime")
-	local pPlayTime = math.floor((getTickCount() - playerTickCount[source]) / 1000)
+	local pPlayTime = math.floor(getRealTime().timestamp - playerTickCount[source])
 	exports.UCDaccounts:SAD(source, "playtime", tonumber(pAccTime + pPlayTime))
 	playerTickCount[source] = nil
 end
@@ -26,10 +26,10 @@ function updateScoreboard()
 	for _, plr in pairs(Element.getAllByType("player")) do
 		if (exports.UCDaccounts:isPlayerLoggedIn(plr)) then 
 			if (not playerTickCount[plr]) then
-				playerTickCount[plr] = getTickCount()
+				playerTickCount[plr] = getRealTime().timestamp
 			end
 			
-			local currentTime = math.floor((getTickCount() - playerTickCount[plr]) / 1000)
+			local currentTime = math.floor(getRealTime().timestamp - playerTickCount[plr])
 			local totalTime   = plr:getData("playtime") + currentTime
 			plr:setData("dxscoreboard_playtime", exports.UCDutil:secondsToHoursMinutes(totalTime))
 		end
@@ -40,7 +40,7 @@ function onStart()
 	for _, plr in pairs(Element.getAllByType("player")) do
 		if (exports.UCDaccounts:isPlayerLoggedIn(plr)) then
 			if (not playerTickCount[plr]) then
-				playerTickCount[plr] = getTickCount()
+				playerTickCount[plr] = getRealTime().timestamp
 			end
 			plr:setData("playtime", exports.UCDaccounts:GAD(plr, "playtime"), true)
 		end
@@ -55,7 +55,7 @@ function onStop()
 	for _, plr in pairs(Element.getAllByType("player")) do
 		if (exports.UCDaccounts:isPlayerLoggedIn(plr)) then		
 			local accPlayTime  = exports.UCDaccounts:GAD(plr, "playtime")
-			local currPlayTime = math.floor((getTickCount() - playerTickCount[plr]) / 1000)
+			local currPlayTime = math.floor(getRealTime().timestamp - playerTickCount[plr])
 			exports.UCDaccounts:SAD(plr, "playtime", tonumber(accPlayTime + currPlayTime))
 			playerTickCount[plr] = nil
 		end
