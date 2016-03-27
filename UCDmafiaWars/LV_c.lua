26e1fdbfc18fd52665579c9e1e58ba41e37c62c5
nokizorque <noki@zorque.xyz>
2016-03-27 22:38:50 +1000

    UCDmafiaWars
    - Fixed debug from LV switching with timer.
    - Fixed to work with new dx side bar.

--- UCDmafiaWars/LV_c.lua
+++ UCDmafiaWars/LV_c.lua
@@ -3,8 +3,8 @@ local t
 addEventHandler("onClientColShapeHit", LV,
 	function (ele, matchingDimension)
 		if (ele.type == "player" and ele == localPlayer and matchingDimension) then
-			exports.UCDdx:add("Welcome to Las Venturas", 255, 255, 0)
-			t = Timer(function () exports.UCDdx:del("Welcome to Las Venturas") end, 2500, 1)
+			exports.UCDdx:add("welcometolv", "Welcome to Las Venturas", 255, 255, 0)
+			t = Timer(function () exports.UCDdx:del("welcometolv") end, 5000, 1)
 		end
 	end
 )
@@ -12,9 +12,9 @@ addEventHandler("onClientColShapeHit", LV,
 addEventHandler("onClientColShapeLeave", LV,
 	function (ele, matchingDimension)
 		if (ele.type == "player" and ele == localPlayer and matchingDimension) then
-			exports.UCDdx:del("Welcome to Las Venturas")
+			exports.UCDdx:del("welcometolv")
 			if (t and isTimer(t)) then
-				t:kill()
+				t:destroy()
 				t = nil
 			end
 		end
