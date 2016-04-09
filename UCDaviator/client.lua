4c7b7d7f8fadd83e570c3083562eb5938f86608d
nokizorque <noki@zorque.xyz>
2016-04-10 04:56:41 +1000

    UCDaviator
    - Added live flight distance.

--- UCDaviator/client.lua
+++ UCDaviator/client.lua
@@ -17,6 +17,7 @@ local prev = {} -- Contains the previous flight itinerary
 local markings = {}
 local pending
 local hasHit
+local dest
 
 function startItinerary(vehicle, seat)
 	if (source == localPlayer and localPlayer.team.name == "Citizens" and localPlayer:getData("Occupation") == "Aviator" and seat == 0 and (vehicle.vehicleType == "Plane" or vehicle.vehicleType == "Helicopter")) then
@@ -33,6 +34,7 @@ addEventHandler("UCDaviator.startItinerary", root, startItinerary)
 
 function cancelItinerary()
 	if (isItinerary()) then
+		exports.UCDdx:del("aviatordist")
 		outputDebugString("cancelling")
 		if (pending and isTimer(pending)) then
 			killTimer(pending)
@@ -47,6 +49,7 @@ function cancelItinerary()
 		markings.marker = nil
 		markings.blip = nil
 		hasHit = nil
+		removeEventHandler("onClientRender", root, displayDistance)
 	end
 end
 addEventHandler("onClientPlayerWasted", localPlayer, cancelItinerary)
@@ -92,7 +95,7 @@ function calculateItinerary()
 			markings.marker = nil
 			markings.blip = nil
 			
-			local dest = destinations["Plane"][airports[curr.airport]][curr.dest]
+			dest = destinations["Plane"][airports[curr.airport]][curr.dest]
 			markings.marker = Marker(dest[1], dest[2], dest[3] - 1, "cylinder", MARKER_SIZE, 255, 215, 0, 120)
 			markings.blip = Blip.createAttachedTo(markings.marker, 41, nil, nil, nil, nil, nil, 0, 65535)
 			
@@ -102,7 +105,7 @@ function calculateItinerary()
 				exports.UCDdx:new("ATC: Take these passgeners to "..airports[curr.airport], 255, 215, 0)
 			end
 		else
-			local dest = destinations["Plane"][airports[curr.airport]][curr.dest]
+			dest = destinations["Plane"][airports[curr.airport]][curr.dest]
 			markings.marker = Marker(dest[1], dest[2], dest[3] - 1, "cylinder", MARKER_SIZE, 255, 215, 0, 120)
 			markings.blip = Blip.createAttachedTo(markings.marker, 41, nil, nil, nil, nil, nil, 0, 65535)
 			
@@ -130,18 +133,18 @@ function calculateItinerary()
 			markings.marker = nil
 			markings.blip = nil
 
-			local dest = destinations["Helicopter"][curr.dest]
+			dest = destinations["Helicopter"][curr.dest]
 			markings.marker = Marker(dest[1], dest[2], dest[3] - 1, "cylinder", MARKER_SIZE, 255, 215, 0, 120)
 			markings.blip = Blip.createAttachedTo(markings.marker, 41, nil, nil, nil, nil, nil, 0, 65535)
 			
-			local suburb, city = getZoneName(dest[1], dest[2], 0), getZoneName(dest[1], dest[2], 0, true)
+			suburb, city = getZoneName(dest[1], dest[2], 0), getZoneName(dest[1], dest[2], 0, true)
 			if (suburb ~= city) then
 				exports.UCDdx:new("PASSENGER: Take me to "..suburb.." in "..city..", please!", 255, 215, 0)
 			else
 				exports.UCDdx:new("PASSENGER: Take me to "..suburb..", please!", 255, 215, 0)
 			end
 		else
-			local dest = destinations["Helicopter"][curr.dest]
+			dest = destinations["Helicopter"][curr.dest]
 			markings.marker = Marker(dest[1], dest[2], dest[3] - 1, "cylinder", MARKER_SIZE, 255, 215, 0, 120)
 			markings.blip = Blip.createAttachedTo(markings.marker, 41, nil, nil, nil, nil, nil, 0, 65535)
 			
@@ -154,6 +157,9 @@ function calculateItinerary()
 		end
 	end
 	
+	if (#getEventHandlers("onClientRender", root, displayDistance) == 0) then
+		addEventHandler("onClientRender", root, displayDistance)
+	end
 	addEventHandler("onClientMarkerHit", markings.marker, onClientMarkerHit)
 end
 
@@ -237,5 +243,20 @@ function onCompleteWaitForPassengers()
 				calculateItinerary()
 			end
 		end
+	end
+end
+
+function displayDistance()
+	if (isItinerary() and dest) then
+		local meas = "m"
+		local p = localPlayer.position
+		local x, y, z = dest[1], dest[2], dest[3]
+		local dist = math.floor(getDistanceBetweenPoints3D(p.x, p.y, p.z, x, y, z))
+		if (dist >= 1000) then
+			dist = exports.UCDutil:mathround(dist / 1000, 1)
+			meas = "km"
+		end
+		
+		exports.UCDdx:add("aviatordist", "Distance: "..tostring(exports.UCDutil:tocomma(dist))..tostring(meas), 255, 215, 0)
 	end
 end
