77a807fdcbfc95ece6ab2644e38dd97536d0b2c4
nokizorque <noki@zorque.xyz>
2016-07-05 19:56:46 +1000

    UCDsql
    - Added forum database connection

--- UCDsql/mysql.lua
+++ UCDsql/mysql.lua
@@ -1,5 +1,10 @@
 -- Have to make this a normal person with ability to do everything but delete, truncate, drop, empty etc
-db = dbConnect("mysql", "dbname=mta;host=noki.zorque.xyz;port=3306", "root", "Network.114")
+local dbname = "mta"
+local host = "noki.zorque.xyz"
+local usr = "root"
+local passwd = "Network.114"
+
+db = dbConnect("mysql", "dbname="..dbname..";host="..host..";port=3306", usr, passwd)
 
 function returnConnection()
 	if (not db) then
@@ -21,4 +26,8 @@ function getConnection()
 		return db
 	end
 	return false
+end
+
+function getForumDatabase()
+	return Connection("mysql", "dbname=smf;host="..host..";port=3306", usr, passwd)
 end
