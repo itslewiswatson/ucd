dee1e72bd31d41d50dc3596a181cc264f312138a
nokizorque <noki@zorque.xyz>
2016-07-09 20:55:27 +1000

    UCDchat
    - Car chat now works with people glued onto a vehicle and its occupants
    - Both attached players and players inside may now chat

--- UCDchat/carchat.lua
+++ UCDchat/carchat.lua
@@ -1,18 +1,31 @@
 function carChat(plr, _, ...)
-	if (not plr.vehicle or not exports.UCDchecking:canPlayerDoAction(plr, "Chat")) then
+	if (not exports.UCDchecking:canPlayerDoAction(plr, "Chat")) then
 		return false
 	end
+	
+	local vehicle
+	local occupants = {}
 	local msg = table.concat({...}, " ")
 	if (msg == "" or msg == " " or msg:gsub(" ", "") == "") then return end
 	if (msg:find("ucd")) then msg = msg:gsub("ucd", "UCD") end
 	
-	local occupants = {}
-	for _, ele in pairs(plr.vehicle:getOccupants()) do
+	if (not plr.vehicle and plr:getAttachedTo()) then
+		vehicle = plr:getAttachedTo()
+		if (vehicle.type ~= "vehicle") then
+			return
+		end
+	elseif (plr.vehicle) then
+		vehicle = plr.vehicle
+	else
+		return
+	end
+	
+	for _, ele in pairs(vehicle:getOccupants()) do
 		if (ele and isElement(ele) and ele.type == "player") then
 			table.insert(occupants, ele)
 		end
 	end
-	for _, ele in pairs(plr.vehicle:getAttachedElements()) do
+	for _, ele in pairs(vehicle:getAttachedElements()) do
 		if (ele and isElement(ele) and ele.type == "player") then
 			table.insert(occupants, ele)
 		end
