1a1ae23328ca49aed9d0922686906ad15bc90697
nokizorque <noki@zorque.xyz>
2016-07-10 18:22:02 +1000

    glue -> UCDglue
    - Added an automatic glue-er
    - Removed some automatic bindings

---
+++ UCDglue/glue_c.lua
@@ -0,0 +1,53 @@
+function glue()
+	if (not localPlayer.vehicle) then
+		local vehicle = getPedContactElement(localPlayer)
+		if (vehicle and vehicle.type == "vehicle") then
+			
+			local px, py, pz = getElementPosition(localPlayer)
+			local vx, vy, vz = getElementPosition(vehicle)
+			local sx = px - vx
+			local sy = py - vy
+			local sz = pz - vz
+			
+			local rotpX = 0
+			local rotpY = 0
+			local rotpZ = getPedRotation(localPlayer)
+			
+			local rotvX, rotvY, rotvZ = getElementRotation(vehicle)
+			
+			local t = math.rad(rotvX)
+			local p = math.rad(rotvY)
+			local f = math.rad(rotvZ)
+			
+			local ct = math.cos(t)
+			local st = math.sin(t)
+			local cp = math.cos(p)
+			local sp = math.sin(p)
+			local cf = math.cos(f)
+			local sf = math.sin(f)
+			
+			local z = ct*cp*sz + (sf*st*cp + cf*sp)*sx + (-cf*st*cp + sf*sp)*sy
+			local x = -ct*sp*sz + (-sf*st*sp + cf*cp)*sx + (cf*st*sp + sf*cp)*sy
+			local y = st*sz - sf*ct*sx + cf*ct*sy
+			
+			local rotX = rotpX - rotvX
+			local rotY = rotpY - rotvY
+			local rotZ = rotpZ - rotvZ
+			
+			local slot = getPedWeaponSlot(localPlayer)
+			
+			triggerServerEvent("gluePlayer", resourceRoot, slot, vehicle, x, y, z, rotX, rotY, rotZ)
+			
+			bindKey("jump", "down", unglue)
+		end
+	end
+end
+addCommandHandler("glue", glue)
+addEvent("UCDglue.glue", true)
+addEventHandler("UCDglue.glue", root, glue)
+
+function unglue()
+	triggerServerEvent("ungluePlayer", resourceRoot)
+	unbindKey("jump", "down", unglue)
+end
+addCommandHandler("unglue", unglue)
---
+++ UCDglue/glue_s.lua
@@ -0,0 +1,34 @@
+local wait = {}
+function clear(plr)
+	wait[plr] = nil
+end
+
+function gluePlayer(slot, vehicle, x, y, z, rotX, rotY, rotZ)
+	if (wait[client]) then
+		return
+	end
+	client:attach(vehicle, x, y, z, rotX, rotY, rotZ)
+	setPedWeaponSlot(client, slot)
+end
+addEvent("gluePlayer",true)
+addEventHandler("gluePlayer", root, gluePlayer)
+
+function ungluePlayer()
+	detachElements(client)
+	wait[client] = true
+	Timer(clear, 1000, 1, client)
+end
+addEvent("ungluePlayer",true)
+addEventHandler("ungluePlayer", root, ungluePlayer)
+
+function onPlayerContactWithVehicle(_, ele)
+	if (ele and ele.type == "vehicle") then
+		if (wait[source]) then
+			return
+		end
+		outputDebugString("got contact - triggering client")
+		triggerClientEvent(source, "UCDglue.glue", source)
+	end
+end
+addEventHandler("onPlayerContact", root, onPlayerContactWithVehicle)
+
