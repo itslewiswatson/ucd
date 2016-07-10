14f85a7966835eae85b8ffaa3fd1b378353db1ca
nokizorque <noki@zorque.xyz>
2016-07-10 22:18:55 +1000

    UCDmods
    - Added some wheel mods
    - Categorized mods into vehicle, skin, etc
    - Added a background downloader that uses md5 sum checks

--- UCDmods/mods.lua
+++ UCDmods/mods.lua
@@ -1,18 +1,60 @@
-function applyMods()	
-	engineImportTXD(engineLoadTXD("staff.txd", true), 217)
-	engineReplaceModel(engineLoadDFF("staff.dff", 217), 217)
-	
-	engineImportTXD(engineLoadTXD("66.txd", true), 66)
-	engineReplaceModel(engineLoadDFF("66.dff", 66), 66)
-	
-	engineImportTXD(engineLoadTXD("275.txd", true), 275)
-	engineReplaceModel(engineLoadDFF("275.dff", 275), 275)
-	
-	engineImportTXD(engineLoadTXD("infernus.txd", true), 411)
-	engineReplaceModel(engineLoadDFF("infernus.dff", 411), 411)
-	
-	engineReplaceModel(engineLoadDFF("hydra.dff", 520), 520)
-	engineReplaceModel(engineLoadDFF("shamal.dff", 519), 519)
-	engineReplaceModel(engineLoadDFF("seaspar.dff", 447), 447)
+function fetchMods()
+	triggerServerEvent("UCDmods.requestMods", resourceRoot)
 end
-addEventHandler( "onClientResourceStart", resourceRoot, applyMods )
+addEventHandler("onClientResourceStart", resourceRoot, fetchMods)
+
+function onReceivedModList(list)
+	local toDownload = {}
+	for _, data in ipairs(list) do
+		if (not File.exists(":UCDmods/"..data[1])) then
+			table.insert(toDownload, data[1]) -- File doesn't exist, download
+			--outputDebugString("[exist] Inserting "..tostring(data[1]))
+		else
+			local f = File(":UCDmods/"..data[1])
+			f.pos = 0
+			local fileData = f:read(f.size)
+			if (hash("md5", fileData):lower() ~= data[3]:lower()) then -- If the hashes do not match (hashes are a mix of lower and upper case for some reason)
+				table.insert(toDownload, data[1])
+				--outputDebugString("[md5] Inserting "..tostring(data[1]))
+			else
+				-- We have the file and it's correct, apply it
+				applyMod(data[1], data[4])
+			end
+			f:close()
+		end
+	end
+	triggerServerEvent("UCDmods.requestDownload", resourceRoot, toDownload)
+	exports.UCDdx:add("ucdmods", "UCDmods - Downloading "..tostring(#toDownload).." items",  200, 0, 200)
+end
+addEvent("UCDmods.onReceivedModList", true)
+addEventHandler("UCDmods.onReceivedModList", root, onReceivedModList)
+
+function onCompleteDownload(mods)
+	outputDebugString("Completed download - "..tostring(#mods).." items")
+	exports.UCDdx:add("ucdmods", "UCDmods - Completed download", 200, 0, 200)
+	Timer(
+		function ()
+			exports.UCDdx:del("ucdmods")
+		end, 5000, 1
+	)
+	for i, data in ipairs(mods) do
+		local path = data[1]
+		local f = File.new(path)
+		f.pos = 0
+		f:write(data[2])
+		f:flush()
+		f:close()
+		applyMod(path, data[3])
+	end
+end
+addEvent("UCDmods.download", true)
+addEventHandler("UCDmods.download", root, onCompleteDownload)
+
+function applyMod(name, id)
+	outputDebugString("Applying "..tostring(name).." on id "..tostring(id))
+	if (name:match("txd")) then
+		engineImportTXD(engineLoadTXD(name, true), id)
+	else
+		engineReplaceModel(engineLoadDFF(name, id), id)
+	end
+end
---
+++ UCDmods/mods_s.lua
@@ -0,0 +1,78 @@
+local mods = {
+	{"staff.txd", 217, "skins"},
+	{"staff.dff", 217, "skins"},
+	{"275.txd", 275, "skins"},
+	{"275.dff", 275, "skins"},
+	{"66.txd", 66, "skins"},
+	{"66.dff", 66, "skins"},
+	
+	{"hydra.dff", 520, "vehicles"},
+	{"shamal.dff", 519, "vehicles"},
+	{"seaspar.dff", 447, "vehicles"},
+	{"infernus.txd", 411, "vehicles"},
+	{"infernus.dff", 411, "vehicles"},
+	
+	{"wheels.txd", 1074, "wheels"},
+	{"wheel_gn1.dff", 1074, "wheels"},
+}
+local mods2  = {}
+local bytes = 0
+
+function cacheMods()
+	--for _, ent in pairs(mods) do
+		for _, data in ipairs(mods) do
+			local type_ = data[3]
+			local fname = data[1]
+			local id = data[2]
+			local path = data[3].."/"..data[1]
+			local f = File(path)
+			if (f) then
+				bytes = bytes + f.size
+				f.pos = 0
+				local modData = f:read(f.size)
+				local modHash = hash("md5", modData)
+				table.insert(mods2, {path, type_, modData, modHash, id}) -- path, modData, modHash, type, id
+				f:close()
+			else
+				outputDebugString("Can't find file '"..tostring(path).."'")
+			end
+		end
+	--end
+	outputDebugString(tostring(bytes / 1000000).."mb")
+end
+addEventHandler("onResourceStart", resourceRoot, cacheMods)
+
+function sendMods(plr)
+	local temp = {}
+	for _, data in ipairs(mods2) do
+		table.insert(temp, {data[1], data[2], data[4], data[5]}) -- path, hash, type
+	end
+	triggerLatentClientEvent(plr, "UCDmods.onReceivedModList", plr, temp)
+end
+
+function requestDownload(list)
+	local temp = {}
+	for k, v in ipairs(list) do
+		for _, data in ipairs(mods2) do
+			if (data[1] == v) then
+				table.insert(temp, {data[1], data[3], data[5]})
+			end
+		end
+	end
+	outputDebugString("requestDownload -> "..tostring(#temp).." items")
+	-- exports.UCDdx:add(client, "ucdmods", "UCDmods - Downloading "..tostring(#temp).." items",  200, 144, 0)
+	triggerLatentClientEvent(client, "UCDmods.download", 150000, false, client, temp)
+end
+addEvent("UCDmods.requestDownload", true)
+addEventHandler("UCDmods.requestDownload", root, requestDownload)
+
+function requestMods()
+	if (#mods2 == 0) then
+		-- mods haven't been cached, wait
+		Timer(sendMods, 1000, 1, client)
+	else
+		sendMods(client)
+	end
+end
+addEvent("UCDmods.requestMods", true)
+addEventHandler("UCDmods.requestMods", root, requestMods)
