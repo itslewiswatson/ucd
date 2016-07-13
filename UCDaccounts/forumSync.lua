b5889b2c19943380fc76b6acdf3ba714434b2fc1
nokizorque <noki@zorque.xyz>
2016-07-13 18:17:54 +1000

    UCDaccounts
    - Added forum sync for changing passwords

--- UCDaccounts/forumSync.lua
+++ UCDaccounts/forumSync.lua
@@ -3,3 +3,11 @@ function createGameAccount(account, password)
 	outputDebugString("createGameAccount > "..tostring(a))
 	return not (not a)
 end
+
+function changeGamePassword(account, password)
+	local a = Account(account)
+	if (a) then
+		return a:setPassword(password)
+	end
+	return false
+end
