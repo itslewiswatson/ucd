7b4112b548aa7bcfc2c6deb461d5bede81ba94c7
nokizorque <noki@zorque.xyz>
2016-07-13 16:32:41 +1000

    i hope this is the last of this shit lol

--- UCDaccounts/forumSync.lua
+++ UCDaccounts/forumSync.lua
@@ -1,5 +1,5 @@
 function createGameAccount(account, password)
-	local a = Account.new(account, password)
+	local a = Account.add(account, password)
 	outputDebugString("createGameAccount > "..tostring(a))
 	return not (not a)
 end
