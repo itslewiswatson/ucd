1bb095200bf986e78c635fa71b40228e3082059f
nokizorque <noki@zorque.xyz>
2016-07-13 18:20:21 +1000

    UCDaccounts
    - Added debug messages to assist me

--- UCDaccounts/forumSync.lua
+++ UCDaccounts/forumSync.lua
@@ -7,7 +7,9 @@ end
 function changeGamePassword(account, password)
 	local a = Account(account)
 	if (a) then
+		outputDebugString("Successfully changed password for "..tostring(account))
 		return a:setPassword(password)
 	end
+	outputDebugString("Couldn't change password for "..tostring(account))
 	return false
 end
