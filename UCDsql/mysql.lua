48609e82e4b24b2e00fb3150b8b8692bc1089ed1
nokizorque <noki@zorque.xyz>
2016-07-13 15:41:50 +1000

    This is the last fucking time

--- UCDsql/mysql.lua
+++ UCDsql/mysql.lua
@@ -1,6 +1,6 @@
 -- Have to make this a normal person with ability to do everything but delete, truncate, drop, empty etc
 local dbname = "mta"
-local host = "ucdmta.com"
+local host = "localhost"
 local usr = "root"
 local passwd = "Network.114"
 
@@ -9,23 +9,30 @@ forum = Connection("mysql", "dbname=forum;host="..host..";port=3306", usr, passw
 
 function returnConnection()
 	if (not db) then
-		outputDebugString("Connection to the MySQL database [via ucdmta.com:3306] failed! Trying noki.zorque.xyz:3306...")
-		db = Connection("mysql", "dbname=mta;host=noki.zorque.xyz;port=3306", "root", "Network.114")
+		outputDebugString("Connection to the MySQL database [via localhost:3306] failed! Trying ucdmta.com:3306...")
+		db = Connection("mysql", "dbname=mta;host=ucdmta.com;port=3306", "root", "Network.114")
 		if (not db) then
-			outputDebugString("Connection to the MySQL database [via noki.zorque.xyz:3306] failed! Trying 192.168.0.2:3306...")
-			db = Connection("mysql", "dbname=mta;host=192.168.0.2;port=3306", "root", "Network.114")
+			outputDebugString("Connection to the MySQL database [via ucdmta.com:3306] failed! Trying noki.zorque.xyz:3306...")
+			db = Connection("mysql", "dbname=mta;host=noki.zorque.xyz;port=3306", "root", "Network.114")
 			if (not db) then
-				outputDebugString("Connection to the MySQL database [via 192.168.0.2:3306] failed!")
+				outputDebugString("Connection to the MySQL database [via noki.zorque.xyz:3306] failed! Trying 192.168.0.2:3306...")
+				db = Connection("mysql", "dbname=mta;host=192.168.0.2;port=3306", "root", "Network.114")
+				if (not db) then
+					outputDebugString("Connection to the MySQL database [via 192.168.0.2:3306] failed!")
+				else
+					outputDebugString("Connection to the MySQL database [via 192.168.0.2:3306] successful!")
+				end
+				return
 			else
-				outputDebugString("Connection to the MySQL database [via 192.168.0.2:3306] successful!")
-			end
+				outputDebugString("Connection to the MySQL database [via noki.zorque.xyz:3306] successful!")
+			end	
 			return
-		else
-			outputDebugString("Connection to the MySQL database [via noki.zorque.xyz:3306] successful!")
-		end	
+		end
+		outputDebugString("Connection to the MySQL database [via ucdmta.com:3306] successful!")
 		return
+	else
+		outputDebugString("Connection to the MySQL database [via localhost:3306] successful!")
 	end
-	outputDebugString("Connection to the MySQL database [via ucdmta.com:3306] successful!")
 end
 addEventHandler("onResourceStart", resourceRoot, returnConnection)
 
