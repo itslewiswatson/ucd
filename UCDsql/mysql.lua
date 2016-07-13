d9d1faf3345a2726d7991b3b4bb7cc62333cdbbc
nokizorque <noki@zorque.xyz>
2016-07-13 15:34:15 +1000

    UCDsql
    - Added server mysql address (we made it)
    - Changed fallback messages

--- UCDsql/mysql.lua
+++ UCDsql/mysql.lua
@@ -1,23 +1,24 @@
 -- Have to make this a normal person with ability to do everything but delete, truncate, drop, empty etc
 local dbname = "mta"
-local host = "noki.zorque.xyz"
+local host = "ucdmta.com"
 local usr = "root"
 local passwd = "Network.114"
 
-db = dbConnect("mysql", "dbname="..dbname..";host="..host..";port=3306", usr, passwd)
+db = Connection("mysql", "dbname="..dbname..";host="..host..";port=3306", usr, passwd)
+forum = Connection("mysql", "dbname=forum;host="..host..";port=3306", usr, passwd)
 
 function returnConnection()
 	if (not db) then
-		outputDebugString("Connection to the MySQL database [via zorque.xyz:3306] failed! Trying 192.168.0.2:3306...")
-		db = dbConnect("mysql", "dbname=mta;host=192.168.0.2;port=3306", "root", "Network.114")
+		outputDebugString("Connection to the MySQL database [via ucdmta.com:3306] failed! Trying noki.zorque.xyz:3306...")
+		db = dbConnect("mysql", "dbname=mta;host=noki.zorque.xyz;port=3306", "root", "Network.114")
 		if (not db) then
-			outputDebugString("Connection to the MySQL database [via 192.168.0.2:3306] failed!")
+			outputDebugString("Connection to the MySQL database [via noki.zorque.xyz:3306] failed! Trying 192.168.0.2:3306...")
 		else
 			outputDebugString("Connection to the MySQL database [via 192.168.0.2:3306] successful!")
 		end	
 		return
 	end
-	outputDebugString("Connection to the MySQL database [via zorque.xyz:3306] successful!")
+	outputDebugString("Connection to the MySQL database [via ucdmta.com:3306] successful!")
 end
 addEventHandler("onResourceStart", resourceRoot, returnConnection)
 
@@ -29,5 +30,5 @@ function getConnection()
 end
 
 function getForumDatabase()
-	return Connection("mysql", "dbname=smf;host="..host..";port=3306", usr, passwd)
+	return forum
 end
