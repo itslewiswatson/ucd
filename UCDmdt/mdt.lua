279cdc3fa85de095929c7dc44ff05b0994a1f23e
nokizorque <noki@zorque.xyz>
2016-07-16 17:04:47 +1000

    UCDmdt
    - Vastly improved the MDT system
    - Added handlers for marking individuals and marking all etc

--- UCDmdt/mdt.lua
+++ UCDmdt/mdt.lua
@@ -3,12 +3,17 @@ local cursor = false
 local blips = {}
 local filters
 local players
+local players2 = {}
+local refreshTimer
+local refreshTimer2
+local markAll = false
 
 function createGUI()
-	MDT.window = GuiWindow(691, 336, 555, 422, "UCD | Mobile Data Terminal (MDT)", false)
+	MDT.window = GuiWindow(691, 336, 555, 402, "UCD | Mobile Data Terminal (MDT)", false)
 	MDT.window.visible = false
 	MDT.window.sizable = false
 	MDT.window.alpha = 1
+	exports.UCDutil:centerWindow(MDT.window)
 
 	MDT.gridlist = GuiGridList(9, 47, 536, 185, false, MDT.window)
 	guiGridListAddColumn(MDT.gridlist, "Player", 0.2)
@@ -16,14 +21,15 @@ function createGUI()
 	guiGridListAddColumn(MDT.gridlist, "Loc", 0.15)
 	guiGridListAddColumn(MDT.gridlist, "Transport", 0.2)
 	guiGridListAddColumn(MDT.gridlist, "Distance", 0.2)
+	guiGridListSetSortingEnabled(MDT.gridlist, false)
 
 	MDT.label[1] = GuiLabel(11, 24, 534, 18, "Click a player's name to toggle marking", false, MDT.window)
 	guiLabelSetHorizontalAlign(MDT.label[1], "center", false)
 	guiLabelSetVerticalAlign(MDT.label[1], "center")
 
-	MDT.button[1] = GuiButton(10, 369, 174, 43, "Mark All", false, MDT.window)
-	MDT.button[2] = GuiButton(190, 369, 174, 43, "Unmark All", false, MDT.window)
-	MDT.button[3] = GuiButton(371, 369, 174, 43, "Close", false, MDT.window)
+	MDT.button[1] = GuiButton(10, 350, 174, 43, "Mark All", false, MDT.window)
+	MDT.button[2] = GuiButton(190, 350, 174, 43, "Unmark All", false, MDT.window)
+	MDT.button[3] = GuiButton(371, 350, 174, 43, "Close", false, MDT.window)
 
 	MDT.label[2] = GuiLabel(17, 237, 157, 20, "Filters", false, MDT.window)
 	guiSetFont(MDT.label[2], "clear-normal")
@@ -84,6 +90,7 @@ function toggleGUI(force)
 			MDT.window.visible = false
 			showCursor(false)
 			cursor = false
+			onUnmarkAll()
 			return false
 		end
 	end
@@ -95,19 +102,29 @@ addEventHandler("onClientGUIClick", MDT.button[3], toggleGUI, false)
 addCommandHandler("policecomputer", toggleGUI)
 addCommandHandler("mdt", toggleGUI)
 bindKey("F5", "up", "mdt")
-
-function onEditFilter()
-	local foundSource = false
-	for i = 1, #MDT.checkbox do
-		if (source == MDT.checkbox[i]) then
-			foundSource = true
-			break
+addEvent("onClientPlayerGetJob", true)
+addEventHandler("onClientPlayerGetJob", root, 
+	function () 
+		if (source == localPlayer and source.team.name ~= "Law") then 
+			toggleGUI(true) 
+		end 
+		if (source == localPlayer and source.team.name == "Law") then
+			if (not refreshTimer2 or not isTimer(refreshTimer2)) then
+				refreshTimer2 = Timer(createGridList, 5000, 0)
+			end
 		end
 	end
-	if (not foundSource) then
-		return
+)
+
+addEventHandler("onClientResourceStart", resourceRoot, 
+	function ()
+		if (localPlayer.team.name == "Law") then
+			refreshTimer2 = Timer(createGridList, 5000, 0)
+		end
 	end
-	
+)
+
+function onEditFilter()
 	filters = {["c"] = {}, ["v"] = {}}
 	-- WL filter
 	if (MDT.checkbox[12].selected) then -- 6 stars plus
@@ -166,6 +183,56 @@ for i = 1, #MDT.checkbox do
 	addEventHandler("onClientGUIClick", MDT.checkbox[i], onEditFilter, false)
 end
 
+function onMarkPlayer()
+	local row = guiGridListGetSelectedItem(MDT.gridlist)
+	if (row and row ~= -1) then
+		local plr = guiGridListGetItemData(MDT.gridlist, row, 1)
+		local isMarked = false
+		for k, v in pairs(players2) do
+			if (v == plr) then
+				isMarked = k
+				break
+			end
+		end
+		if (isMarked) then
+			table.remove(players2, k)
+			refreshBlips()
+			return
+		end
+		
+		table.insert(players2, plr)
+		refreshBlips()
+	end
+end
+addEventHandler("onClientGUIClick", MDT.gridlist, onMarkPlayer)
+
+function onMarkAll()
+	onEditFilter()
+	--applyFilters()
+	--refreshBlips()
+	if (not refreshTimer or not isTimer(refreshTimer)) then
+		refreshTimer = Timer(applyFilters, 2000, 0)
+	end
+	markAll = true
+	players2 = players
+	refreshBlips()
+end
+addEventHandler("onClientGUIClick", MDT.button[1], onMarkAll, false)
+
+function onUnmarkAll()
+	for plr, blip in pairs(blips) do
+		blip:destroy()
+		blips[plr] = nil
+	end
+	if (refreshTimer or isTimer(refreshTimer)) then
+		refreshTimer:destroy()
+		refreshTimer = nil
+	end
+	markAll = false
+	players2 = {}
+end
+addEventHandler("onClientGUIClick", MDT.button[2], onUnmarkAll, false)
+
 function getAllWantedPlayers()
 	local temp = {}
 	for _, plr in ipairs(Element.getAllByType("player")) do
@@ -177,7 +244,6 @@ function getAllWantedPlayers()
 end
 
 function applyFilters()
-	players = {}
 	local filterCount = 0
 	
 	if (filters["c"] and #filters["c"] > 0) then
@@ -193,6 +259,8 @@ function applyFilters()
 		filterCount = filterCount + 1
 	end
 	--outputDebugString("filterCount > "..filterCount)
+	
+	players = {}
 	
 	for _, plr in ipairs(getAllWantedPlayers()) do
 		local w, v, d, c
@@ -277,6 +345,7 @@ function applyFilters()
 		end
 	end
 	createGridList()
+	refreshBlips()
 end
 
 function createGridList()
@@ -306,29 +375,37 @@ function createGridList()
 			guiGridListSetItemText(MDT.gridlist, row, 3, tostring(exports.UCDutil:getCityZoneFromXYZ(x, y, z)), false, false)
 			guiGridListSetItemText(MDT.gridlist, row, 4, tostring(type2), false, false)
 			guiGridListSetItemText(MDT.gridlist, row, 5, tostring(math.floor(getDistanceBetweenPoints3D(localPlayer.position, x, y, z))), false, false)
+			guiGridListSetItemData(MDT.gridlist, row, 1, plr)
 		end
 	else
 		players = getAllWantedPlayers()
 		createGridList()
 	end
 end
+createGridList()
+
+function onPlayerWPChange()
+	applyFilters()
+end
+addEvent("UCDmdt.onPlayerWPChange", true)
+addEventHandler("UCDmdt.onPlayerWPChange", root, onPlayerWPChange)
 
 function refreshBlips()
-	for i = 0, guiGridListGetRowCount(MDT.gridlist) - 1 do
-		local plr = guiGridListGetItemData(MDT.gridlist, i, 1)
-		if (plr and plr:getData("w") and plr:getData("w") > 0) then
-			if (blips[plr]) then
-				blips[plr]:destroy()
-				blips[plr] = nil
-			end
-			blips[plr] = Blip.createAttachedTo(plr, 20)
-		else
-			if (blips[plr]) then
-				blips[plr]:destroy()
-				blips[plr] = nil
-			end
-			guiGridListRemoveRow(i)
+	for _, plr in ipairs(Element.getAllByType("player")) do
+		if (blips[plr]) then
+			blips[plr]:destroy()
+			blips[plr] = nil
 		end
+	end
+	if (players2) then
+		--if (markAll) then
+			for _, plr in ipairs(players2) do
+				blips[plr] = Blip.createAttachedTo(plr, 20)
+			end
+		--end
+	else
+		--players = getAllWantedPlayers()
+		--refreshBlips()
 	end
 end
 
