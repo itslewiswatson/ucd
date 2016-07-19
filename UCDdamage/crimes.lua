9c4a9bd9c3a2f72d09e31f97aa0a909d55acb830
nokizorque <noki@zorque.xyz>
2016-07-19 20:10:26 +1000

    UCDdamage
    - Added a check for if a player is in LV

--- UCDdamage/crimes.lua
+++ UCDdamage/crimes.lua
@@ -3,38 +3,40 @@ local police = Team.getFromName("Law")
 
 function crime(attacker, killer, _, loss)
     local criminal
-    if (isElement(attacker)) then
-        if (attacker.type == "player") then
-            if (attacker.team ~= police and source.team == police) then
-                criminal = attacker
-            end
-        elseif (attacker.type == "vehicle") then
-            if (attacker.occupant and attacker.occupant.team ~= police and source.team == police) then
-                criminal = attacker.occupant
-            end    
-        end
-    elseif (isElement(killer)) then
-        if (killer.type == "player") then
-            if killer.team ~= police and source.team == police then
-                criminal = killer
-            end
-        elseif (killer.type == "vehicle") then
-            if (killer.occupant and killer.occupant.team ~= police and source.team == police) then
-                criminal = killer.occupant
-            end
-        end
-    end
-    if (criminal) then
-		if (eventName == "onPlayerWasted") then
-			exports.UCDwanted:addWantedPoints(criminal, 30)
-		else
-			if (not spam[criminal] or spam[criminal] and getTickCount() - spam[criminal] > 5000) then
-				loss = (loss < 5 and 5) or math.ceil(loss / 3)
-				exports.UCDwanted:addWantedPoints(criminal, loss)
-				spam[criminal] = getTickCount()
+	if (not exports.UCDmafiaWars:isElementInLV(source)) then
+		if (isElement(attacker)) then
+			if (attacker.type == "player") then
+				if (attacker.team ~= police and source.team == police) then
+					criminal = attacker
+				end
+			elseif (attacker.type == "vehicle") then
+				if (attacker.occupant and attacker.occupant.team ~= police and source.team == police) then
+					criminal = attacker.occupant
+				end    
+			end
+		elseif (isElement(killer)) then
+			if (killer.type == "player") then
+				if killer.team ~= police and source.team == police then
+					criminal = killer
+				end
+			elseif (killer.type == "vehicle") then
+				if (killer.occupant and killer.occupant.team ~= police and source.team == police) then
+					criminal = killer.occupant
+				end
 			end
 		end
-    end
+		if (criminal) then
+			if (eventName == "onPlayerWasted") then
+				exports.UCDwanted:addWantedPoints(criminal, 30)
+			else
+				if (not spam[criminal] or spam[criminal] and getTickCount() - spam[criminal] > 5000) then
+					loss = (loss < 5 and 5) or math.ceil(loss / 3)
+					exports.UCDwanted:addWantedPoints(criminal, loss)
+					spam[criminal] = getTickCount()
+				end
+			end
+		end
+	end
 end
 addEventHandler("onPlayerDamage", root, crime)
 addEventHandler("onPlayerWasted", root, crime)
