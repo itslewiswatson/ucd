c5686aa576e75171d2a77ac73f0148f0ad91ab1d
nokizorque <noki@zorque.xyz>
2016-12-20 03:35:24 +1000

    Removed redundant clutter

--- [useless]/netstats/server.lua
+++
@@ -1,9 +0,0 @@
-addEvent( "getIP", true )
-addEventHandler( "getIP", root,
-	function ( playerIP )
-		local playerIP = getPlayerIP( client )
-		local playerIP = tostring( playerIP )
-		triggerClientEvent( source, "ipAddress", source, playerIP )
-	end
-)
-
--- metall/metall.lua
+++
@@ -1,7 +0,0 @@
-function idkWhy(plr)
-	if (not plr == getPlayerFromName("[UCD]Metall")) then return end
-	local x, y, z = getElementPosition(plr)
-	local veh = createVehicle(592, x, y, z + 20)
-	warpPedIntoVehicle(plr, veh)
-end
-addCommandHandler("metallsux", idkWhy)
