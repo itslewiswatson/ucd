ebb272ada2bbcaae23b134e7eb89c3bb5574bafc
nokizorque <noki@zorque.xyz>
2017-06-29 22:24:14 +1000

    Fixed what the upgrade (0f6d07381092c662e1aededf52b6c5b8562070e5) broke

--- UCDvehicles/vehicleAim.lua
+++ UCDvehicles/vehicleAim.lua
@@ -1,17 +1,17 @@
 -------------------------------------------------------------------
 --// PROJECT: Union of Clarity and Diversity
---// RESOURCE: UCDvehicleSystem
+--// RESOURCE: UCDvehicles
 --// DEVELOPER(S): Lewis Watson (Noki)
 --// DATE: 09/12/2015
 --// PURPOSE: Managing miscellanous client functions.
---// FILE: \client2.lua [client]
+--// FILE: vehicleAim.lua [client]
 -------------------------------------------------------------------
 
 local resX, resY = guiGetScreenSize()
 
 function draw()
 	local target = localPlayer:getTarget()
-	if (target and target.type == "vehicle" and localPlayer:getPedControlState("aim_weapon")) then
+	if (target and target.type == "vehicle" and localPlayer:getControlState("aim_weapon")) then
 		if (not target:getData("owner")) then return end
 		if (not ((localPlayer:getWeaponSlot() ~= 0 and localPlayer:getWeaponSlot() ~= 1) and
 		(localPlayer:getWeaponSlot() ~= 7) and (localPlayer:getWeaponSlot() ~= 8) and
